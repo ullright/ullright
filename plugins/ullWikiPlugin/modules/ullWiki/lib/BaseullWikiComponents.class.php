0e37b4d57e3aee34c088154b431df3fda79a95f4
Denny Reeh <denny.reeh@gmail.com>
2008-07-28 20:26:46 +0000

    Ticket #182, ullWiki Module Name

--- plugins/ullWikiPlugin/modules/ullWiki/lib/BaseullWikiComponents.class.php
+++ plugins/ullWikiPlugin/modules/ullWiki/lib/BaseullWikiComponents.class.php
@@ -4,18 +4,18 @@ class BaseullWikiComponents extends sfComponents
 {
   public function executeUllWikiHeader() {
     
-    $show_link = array('action' => 'show', 'docid' => $this->ullwiki->getDocid());
+    $show_link = array('module' => 'ullWiki', 'action' => 'show', 'docid' => $this->ullwiki->getDocid());
     $this->edit_link = array('module' => 'ullWiki', 'action' => 'edit', 'docid' => $this->ullwiki->getDocid());
-    
+
     if ($this->getActionName() == 'show') {
-      
+
       // show edit link only with sufficent rights
       if (UllUserPeer::userHasGroup(1)) {
         $this->subject_link = $this->edit_link;
       } else {
         $this->subject_link = $show_link;
       }
-      
+
     } else {
 //      $this->subject_link = 'wiki/show?id='.$this->wiki->getID().'&cursor='.$this->cursor;          
       $this->subject_link = $show_link;
