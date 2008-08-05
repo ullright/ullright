a5dbab9dc24045f88321fd958c0cbe6fc4248c54
Klemens Ullmann-Marx <klemens@ull.at>
2008-08-05 12:21:54 +0000

    converted UpgradeSf10ToSf11Task to shellscript

--- plugins/ullCorePlugin/batch/ullright_upgrade_sf10_to_sf11.sh
+++ plugins/ullCorePlugin/batch/ullright_upgrade_sf10_to_sf11.sh
@@ -5,11 +5,16 @@ REPO_URL="https://ssl.ull.at/svn/ullright/trunk"
 
 echo "Removing old config files..."
 rm config/i18n.yml
+rm config/config.php
 rm apps/myApp/config/i18n.yml
 rm apps/myApp/config/logging.yml
+rm apps/myApp/config/config.php
 
 echo ""
 echo "Getting new config files from ullright repository"
+
+MYPATH="symfony"
+svn export $REPO_URL/$MYPATH $MYPATH
 
 MYPATH="config/ProjectConfiguration.class.php"
 svn export $REPO_URL/$MYPATH $MYPATH
@@ -19,5 +24,9 @@ svn export $REPO_URL/$MYPATH $MYPATH
 
 MYPATH="apps/myApp/config/factories.yml"
 svn export $REPO_URL/$MYPATH $MYPATH
+
+echo ""
+echo "running symfony project:upgrade1.1 task"
+php symfony project:upgrade1.1
 
 echo Done!
