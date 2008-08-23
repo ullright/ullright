5330ca647c945dc8e390a47767abcffc92a11335
Klemens Ullmann-Marx <klemens@ull.at>
2008-08-23 20:49:33 +0000

    prototype of new ullTableTool - work in progress - foreign keys and validation

--- plugins/ullCorePlugin/modules/ullTableTool/lib/form/TableToolRequestForm.php
+++ plugins/ullCorePlugin/modules/ullTableTool/lib/form/TableToolRequestForm.php
@@ -6,16 +6,16 @@ class TableToolRequestForm extends sfForm
   public function configure()
   {
     $this->setWidgets(array(
-          'id'          => new sfWidgetFormInputHidden
-        , 'table_name'  => new sfWidgetFormInputHidden
+        'id'          => new sfWidgetFormInputHidden,
+        'table_name'  => new sfWidgetFormInputHidden,
         
     ));
     
     $this->widgetSchema->setNameFormat('table_tool[%s]');
   
     $this->setValidators(array(
-          'id'          => new sfValidatorPass
-        , 'table_name'  => new sfValidatorPass
+        'id'          => new sfValidatorInteger(),
+        'table_name'  => new sfValidatorString(),
     ));
   }
   
