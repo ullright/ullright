2d36e5fa37af6d687e3bbc69699968b97061a4a8
Klemens Ullmann-Marx <klemens@ull.at>
2008-11-14 10:21:42 +0000

    ullWiki: _headerSearch.php: renamed the label of the search button to ">" to prevend collision with the modules' search buttons

--- plugins/ullWikiPlugin/modules/ullWiki/templates/_headerSearch.php
+++ plugins/ullWikiPlugin/modules/ullWiki/templates/_headerSearch.php
@@ -1,5 +1,6 @@
 <form class="inline" name="ull_header_wiki_search_form" method="post" action="<?=url_for('ullWiki/list')?>">
   <?php echo $form['search']->render() ?>
   <?php echo $form['fulltext']->render() ?>
-  <?php echo ull_button_to_function(__('Search', null, 'common'), 'document.ull_header_wiki_search_form.submit();', 'ull_js_observer_confirm=true style=margin:0;') ?>
+  <?php //echo ull_button_to_function(__('Search', null, 'common'), 'document.ull_header_wiki_search_form.submit();', 'ull_js_observer_confirm=true style=margin:0;') ?>
+  <?php echo ull_button_to_function('&gt;', 'document.ull_header_wiki_search_form.submit();', 'ull_js_observer_confirm=true style=margin:0;') ?>
 </form>
