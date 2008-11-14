c8ca4fb5a6f83f233dcac2bc4cd9aa4cd39d199c
Klemens Ullmann-Marx <klemens@ull.at>
2008-11-14 11:21:35 +0000

    fixed typo

--- plugins/ullWikiPlugin/modules/ullWiki/templates/_headerSearch.php
+++ plugins/ullWikiPlugin/modules/ullWiki/templates/_headerSearch.php
@@ -2,5 +2,5 @@
   <?php echo $form['search']->render() ?>
   <?php echo $form['fulltext']->render() ?>
   <?php //echo ull_button_to_function(__('Search', null, 'common'), 'document.ull_header_wiki_search_form.submit();', 'ull_js_observer_confirm=true style=margin:0;') ?>
-  <?php echo ull_button_to_function('&gt;%$gt;', 'document.ull_header_wiki_search_form.submit();', 'ull_js_observer_confirm=true style=margin:0;') ?>
+  <?php echo ull_button_to_function('&gt;&gt;', 'document.ull_header_wiki_search_form.submit();', 'ull_js_observer_confirm=true style=margin:0;') ?>
 </form>
