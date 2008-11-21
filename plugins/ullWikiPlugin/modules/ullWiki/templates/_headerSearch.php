11b202b55f99554812e77035b477b1f7c58f4d4c
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2008-11-21 12:53:41 +0000

    ullWiki: adapted search box to use new helper

--- plugins/ullWikiPlugin/modules/ullWiki/templates/_headerSearch.php
+++ plugins/ullWikiPlugin/modules/ullWiki/templates/_headerSearch.php
@@ -2,5 +2,6 @@
   <?php echo $form['search']->render() ?>
   <?php echo $form['fulltext']->render() ?>
   <?php //echo ull_button_to_function(__('Search', null, 'common'), 'document.ull_header_wiki_search_form.submit();', 'ull_js_observer_confirm=true style=margin:0;') ?>
-  <?php echo ull_button_to_function('&gt;&gt;', 'document.ull_header_wiki_search_form.submit();', 'ull_js_observer_confirm=true style=margin:0;') ?>
+  <?php echo ull_link_to_function(ull_image_tag('search', null, null, null, 'ullWiki'), 'document.ull_header_wiki_search_form.submit();', 'ull_js_observer_confirm=true style=margin:0;') ?>
+  <?php //echo ull_button_to_function('&gt;&gt;', 'document.ull_header_wiki_search_form.submit();', 'ull_js_observer_confirm=true style=margin:0;') ?>
 </form>
