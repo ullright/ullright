4bd1d493a04be64d7f80459709ec00a1822feae7
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2008-11-21 13:24:18 +0000

    New layout

--- plugins/ullThemeNG/templates/layout.php
+++ plugins/ullThemeNG/templates/layout.php
@@ -18,32 +18,73 @@
     , 'last'
     , array('media' => 'all')
   );
+
 ?>
 </head>
 <body>
+<div id="indicator" style="display: none"></div> <!-- Ajax indicatior -->
+<div id="container">
 
-  <div id="indicator" style="display: none"></div> <!-- Ajax indicatior -->
+	<div id="sidebar">
+		<div id="nav_logo">
+		  <?php echo ull_link_to(
+		              image_tag('/' . $layout . '/images/logo_120', 'alt="logo"')
+		            , '@homepage'
+		            , 'ull_js_observer_confirm=true'
+		            ); ?> 
+		        </div>
+		<div id="sidebar_content">
+		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam convallis dapibus risus. Donec ligula nisi, eleifend vitae, sollicitudin eget, cursus a, turpis. Aliquam interdum massa. Duis nec ligula eu arcu rutrum molestie. In arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra...
+		</div>
+	</div>
+	
+	<div id="content">
+	<div id="nav_top">
 
-  <div id="container">
-    
-    <div id="content">
-        
-      <div id="nav_top">
-      
-        <div id="nav_logo">
-          <?php echo ull_link_to(
-              image_tag('/' . $layout . '/images/logo', 'alt="logo"')
-            , '@homepage'
-            , 'ull_js_observer_confirm=true'
-            ); ?> 
-        </div>
-
-        <div id="nav_syslinks_container">
-
-          <div id="nav_syslinks">
-            <?php include_component('ullUser', 'headerLogin'); ?>
+       <div id='nav_links'>             
+          <ul class="nav_top_links">            
             
-             &nbsp; | &nbsp;
+            <!--<li><?php //echo ull_link_to(__('Home', null, 'common'), 'homepage', 'ull_js_observer_confirm=true'); ?></li> -->
+            
+            <li id='nav_link_workflow'>
+            <?php
+                echo image_tag("/ullFlowThemeNGPlugin/images/ull_flow_32x32", 'alt="Workflow"') . "<br />";  
+                echo ull_link_to(__('Workflows', null, 'common'), 'ullFlow/index', 'ull_js_observer_confirm=true');
+              ?>
+            </li>
+            
+            <li id='nav_link_tickets'>
+            <?php
+              echo image_tag("/ullFlowThemeNGPlugin/images/ull_flow_app_icons/bug_tracking_32x32", 'alt="Tickets"') . "<br />";
+              echo ull_link_to(__('Active tickets', null, 'common'), 'ullFlow/tabular?app=helpdesk_tool&order=priority&order_dir=asc','ull_js_observer_confirm=true');
+            ?>
+            </li>
+            
+            <li id='nav_link_todo'>
+            <?php
+              echo image_tag("/ullFlowThemeNGPlugin/images/ull_flow_app_icons/todo_32x32", 'alt="Todo"') . "<br />";
+              echo ull_link_to(__('My todo list', null, 'common'),'ullFlow/tabular?query=to_me&order=priority&order_dir=asc', 'ull_js_observer_confirm=true');
+            ?>
+            </li>
+            
+            <li id='nav_link_wiki'>
+            <?php
+              echo image_tag("/ullWikiThemeNGPlugin/images/ull_wiki_32x32", 'alt="Wiki"') . "<br />"; 
+              echo ull_link_to(__('Wiki', null, 'common'), 'ullWiki/list', 'ull_js_observer_confirm=true');
+            ?>
+            </li>
+            <?php 
+              if (UllUserTable::hasGroup('MasterAdmins')) {
+                echo "<li id=\"nav_link_admin\">" .
+                 image_tag("/ullCoreThemeNGPlugin/images/ull_admin_32x32", 'alt="Admin"') . "<br />" .
+                 ull_link_to(__('Admin'), 'ullAdmin/index', 'ull_js_observer_confirm=true') . '</li>';
+              }
+            ?>
+          </ul>
+
+         <div id="nav_syslinks_container">
+
+          <div id="nav_langlinks">      
             <?php
               $culture = $sf_user->getCulture();
               $language = substr($culture,0,2);
@@ -59,58 +100,30 @@
               }
             ?>
           </div>
-
-          <div id="nav_searchbox">                   
+          <br />
+          <div id="nav_searchbox">
             <?php include_component('ullWiki', 'headerSearch'); ?>
           </div>
 
+          <br />
+           <div id="nav_loginbox">
+            <?php include_component('ullUser', 'headerLogin'); ?>
+            </div>
+
          </div>
-
-
-
-        <div id='nav_links'>             
-          <ul class="nav_top_links">            
-            <li><?php echo ull_link_to(__('Home', null, 'common'),           '@homepage',                                                             'ull_js_observer_confirm=true'); ?></li>
-            <li><?php echo ull_link_to(__('Workflows', null, 'common'),      'ullFlow/index',                                                       'ull_js_observer_confirm=true'); ?></li>
-            <li><?php echo ull_link_to(__('Active tickets', null, 'common'), 'ullFlow/tabular?app=helpdesk_tool&order=priority&order_dir=asc','ull_js_observer_confirm=true'); ?></li>
-            <li><?php echo ull_link_to(__('My todo list', null, 'common'),   'ullFlow/tabular?query=to_me&order=priority&order_dir=asc',      'ull_js_observer_confirm=true'); ?></li>
-            <li><?php echo ull_link_to(__('Wiki', null, 'common'),           'ullWiki/list',                                                  'ull_js_observer_confirm=true'); ?></li>
-            <?php 
-              if (UllUserTable::hasGroup('MasterAdmins')) {
-                echo '<li>' . ull_link_to(__('Admin'), 'ullAdmin/index', 'ull_js_observer_confirm=true') . '</li>';
-              } 
-            ?>
-
-          </ul>
-        </div>
-
-        <?php
-//        ullCoreTools::printR($sf_request->getLanguages());
-//        echo 'Current User Culture: ', $sf_user->getCulture();
-          //$context = sfContext::getInstance();
-          
-//          ullCoreTools::printR($sf_user->getAttributeHolder());
-          
-          //ullCoreTools::printR($sf_request);
-        ?>
-        
-        <div class='clear'></div> <!-- to force the parent-box to enclose the floating divs -->
-           
-      </div> <!-- end of nav_top -->
-      
-      <div id='content_main'>
+       </div>
+      <div class='clear'></div> <!-- to force the parent-box to enclose the floating divs -->
+     </div>
+     
+     <div id='content_main'>
         <?php echo $sf_data->getRaw('sf_content') ?>
       </div> <!-- end of content_main -->
-              
-    </div> <!-- end of content -->
-  
-    <div id="footer">
+      <div id="footer">
       <div id="footer_copyright">© 2007-2008 by Klemens Ullmann</div>
       <div id="footer_links">powered by <?php echo ull_link_to('ull.at', 'http://www.ull.at', 'ull_js_observer_confirm=true'); ?></div>
       <div class='clear'></div> <!-- to force the parent-box to enclose the floating divs -->
     </div>  
-    
-  </div> <!-- end of container -->
-  
+  </div>
+</div>
 </body>
 </html>
