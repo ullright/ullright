3f07b03d02c7bb3cd35352806742e8cc0eeeb98c
Klemens Ullmann-Marx <klemens@ull.at>
2008-12-03 09:09:14 +0000

    refactored ullWidgetCheckbox

--- plugins/ullCorePlugin/lib/form/widget/ullWidgetCheckbox.php
+++ plugins/ullCorePlugin/lib/form/widget/ullWidgetCheckbox.php
@@ -6,13 +6,13 @@ class ullWidgetCheckbox extends ullWidget
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
     $image = 'checkbox_unchecked';
-    if ($value) {
+    
+    if ($value) 
+    {
       $image = 'checkbox_checked';
     }
-    return $this->renderContentTag('img', null,
-      array('src'   => '/'. sfConfig::get('app_theme', 'ullThemeDefault').'/images/forms/'.$image.'.png',
-            'alt'   => __($image),
-            'title' => __($image)));
+    
+    return ull_image_tag($image, array(), 9, 9, 'ullCore');
   }
 
 }
