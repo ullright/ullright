1f29fd393092b46051852b2630ff4ce6bd18e0ef
Denny Reeh <denny.reeh@gmail.com>
2008-12-05 21:25:37 +0000

    training whitespaces

--- plugins/ullCorePlugin/lib/form/widget/ullWidgetCheckbox.php
+++ plugins/ullCorePlugin/lib/form/widget/ullWidgetCheckbox.php
@@ -2,16 +2,16 @@
 
 class ullWidgetCheckbox extends ullWidget
 {
-  
+
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
     $image = 'checkbox_unchecked';
-    
-    if ($value) 
+
+    if ($value)
     {
       $image = 'checkbox_checked';
     }
-    
+
     return ull_image_tag($image, array(), 9, 9, 'ullCore');
   }
 
