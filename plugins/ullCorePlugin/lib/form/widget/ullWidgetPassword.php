af568da218b86a243c2aaefd77424ea4da21539f
Denny Reeh <denny.reeh@gmail.com>
2008-12-10 21:35:06 +0000

    return empty string if no value is set

--- plugins/ullCorePlugin/lib/form/widget/ullWidgetPassword.php
+++ plugins/ullCorePlugin/lib/form/widget/ullWidgetPassword.php
@@ -2,11 +2,16 @@
 
 class ullWidgetPassword extends ullWidget
 {
-  
+
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
-    if ($value) {
+    if ($value)
+    {
       $value = '********';
+    }
+    else
+    {
+      $value = '';
     }
     return $value;
   }
