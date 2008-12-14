29dcecb43897abd093bb1b6baf4ac9d95f6ae7a8
Denny Reeh <denny.reeh@gmail.com>
2008-12-14 20:26:37 +0000

    trailing whitespaces

--- plugins/ullCorePlugin/lib/form/widget/ullMetaWidgetUllSelect.class.php
+++ plugins/ullCorePlugin/lib/form/widget/ullMetaWidgetUllSelect.class.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * ullMetaWidgetUllSelect 
- * 
- * 
+ * ullMetaWidgetUllSelect
+ *
+ *
  * available options:
- *  'ull_select' => string, slug of a UllSelect 
+ *  'ull_select' => string, slug of a UllSelect
  *  'add_empty'  => boolean, true to include an empty entry
  */
 class ullMetaWidgetUllSelect extends ullMetaWidget
@@ -12,13 +12,13 @@ class ullMetaWidgetUllSelect extends ullMetaWidget
   public function __construct($columnConfig, sfForm $form)
   {
     parent::__construct($columnConfig, $form);
-    
+
     if (!isset($this->columnConfig['widgetOptions']['ull_select']))
     {
       throw new InvalidArgumentException('option "ull_select" is required');
     }
   }
-  
+
   protected function addToForm()
   {
     if ($this->isWriteMode())
@@ -31,15 +31,15 @@ class ullMetaWidgetUllSelect extends ullMetaWidget
         ->from('UllSelectChild a')
         ->where('a.UllSelect.slug = ?', $ullSelect)
       ;
-      
+
       $this->columnConfig['widgetOptions']['model'] = 'UllSelectChild';
       $this->columnConfig['widgetOptions']['order_by'] = array('sequence', 'asc');
       $this->columnConfig['widgetOptions']['query'] = $q;
-      
+
       $this->columnConfig['validatorOptions']['model'] = 'UllSelectChild';
-      $this->columnConfig['validatorOptions']['query'] = $q;      
-      
-      
+      $this->columnConfig['validatorOptions']['query'] = $q;
+
+
       $this->addWidget(new sfWidgetFormDoctrineSelect(
         $this->columnConfig['widgetOptions'],
         $this->columnConfig['widgetAttributes']
@@ -49,13 +49,13 @@ class ullMetaWidgetUllSelect extends ullMetaWidget
     else
     {
       unset($this->columnConfig['widgetOptions']['add_empty']);
-      
+
       $this->addWidget(new ullWidgetUllSelect($this->columnConfig['widgetOptions'], $this->columnConfig['widgetAttributes']));
       $this->addValidator(new sfValidatorPass());
     }
-    
+
     //    var_dump($columnConfig);die;
-  }  
+  }
 }
 
 ?>
--- plugins/ullCorePlugin/lib/form/widget/ullWidgetUllSelect.php
+++ plugins/ullCorePlugin/lib/form/widget/ullWidgetUllSelect.php
@@ -2,7 +2,7 @@
 
 class ullWidgetUllSelect extends sfWidgetForm
 {
-  
+
   protected function configure($options = array(), $attributes = array())
   {
     $this->addRequiredOption('ull_select');
@@ -10,25 +10,25 @@ class ullWidgetUllSelect extends sfWidgetForm
 
     parent::configure($options, $attributes);
   }
-  
+
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
     if (empty($value))
     {
       return '';
     }
-    
+
     $q = new Doctrine_Query;
     $q
       ->from('UllSelectChild a')
       ->where('a.id = ?', $value)
     ;
-    
+
 //    var_dump($q->getQuery());die;
-    
+
     return $q->execute()->getFirst()->label;
   }
-  
+
 }
 
 ?>
