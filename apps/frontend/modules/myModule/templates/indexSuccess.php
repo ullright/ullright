0328da4d732e295020263fb0de10eea799c91f5a
Klemens Ullmann-Marx <klemens@ull.at>
2008-12-17 17:35:44 +0000

    added login information to welcome msg

--- apps/frontend/modules/myModule/templates/indexSuccess.php
+++ apps/frontend/modules/myModule/templates/indexSuccess.php
@@ -1,3 +1,7 @@
 <br />
 <h1>Welcome, I hope you're ullright!</h1>
-If you can see this, the basic configuration is working fine.
+<p>If you can see this, the basic configuration is working fine.</p>
+<br/>
+<br/>
+<br/>
+<p>Please login as user admin/admin.</p>
