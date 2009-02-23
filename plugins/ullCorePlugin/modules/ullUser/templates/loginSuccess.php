81030a27551d8debfd5a43cd097f8b606d61a493
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2009-02-23 14:34:15 +0000

    Adapted login to use edit view style (fixes #400)

--- plugins/ullCorePlugin/modules/ullUser/templates/loginSuccess.php
+++ plugins/ullCorePlugin/modules/ullUser/templates/loginSuccess.php
@@ -1,20 +1,39 @@
+<br />
+<h2><?php echo __('Log in', null, 'common'); ?></h2>
+<br />
 <?php echo form_tag('ullUser/login') ?>
-
-  <br /><br /><br />
   
   <?php if (isset($msg)): ?>
     <em><?php echo $msg; ?></em><br /><br />
   <?php endif; ?>
 
-  <table>
+  <div class="edit_container">
+    <table class="edit_table">
+      <tbody>
     <?php echo $form ?>
+    </tbody>
   </table>
 
-  <?php echo javascript_tag('document.getElementById("login_js_check").value = 1;'); ?>
-  <?php echo javascript_tag('document.getElementById("login_username").focus();'); ?>
+<?php echo javascript_tag('document.getElementById("login_js_check").value = 1;'); ?>
+<?php echo javascript_tag('document.getElementById("login_username").focus();'); ?>
+  
+<br />
+    <div class='edit_action_buttons color_light_bg'>
+      <div class='edit_action_buttons_left'>
+        <?php echo submit_tag(__('Log in')); ?>
+      </div>
+            <div class='edit_action_buttons_right'>
+        <ul>
+          <li>
+          <?php echo ull_link_to(__('Cancel', null, 'common'), '@homepage') ?>
+          </li>
+        </ul>
+      </div>
+      <div class="clear"></div>
+    </div>
+  </div>
   
   <br />
-  <?php echo submit_tag(__('Log in')); ?>
  
 </form>
 
