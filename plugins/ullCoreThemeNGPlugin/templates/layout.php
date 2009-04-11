ecc53f25d1543865a731a70474f4e5c457c62255
Klemens Ullmann-Marx <klemens@ull.at>
2009-04-11 11:51:33 +0000

    simplified and cleaned layout & css. closes #396

--- plugins/ullCoreThemeNGPlugin/templates/layout.php
+++ plugins/ullCoreThemeNGPlugin/templates/layout.php
@@ -15,15 +15,19 @@
   $path =  '/ullCoreTheme' . sfConfig::get('app_theme_package', 'NG') . "Plugin/css/main.css";
   sfContext::getInstance()->getResponse()->addStylesheet($path, 'first', array('media' => 'all'));
 ?>
+
 <!-- html head slot -->
 <?php include_slot('html_head') ?>
 <!-- end of html head slot -->
+
 <?php 
   sfContext::getInstance()->getResponse()->addStylesheet('custom_override.css', 'last', array('media' => 'all'));
 ?>
 </head>
+
+
 <body>
-<div id="indicator" style="display: none"></div> <!-- Ajax indicatior -->
+
 <div id="container">
 
 	<div id="sidebar">
@@ -53,61 +57,74 @@
         }
      ?>
 		</div>
-	</div>
+		
+	</div> <!-- end of sidebar -->
 	
-	<!-- Contains main navigation and content -->
+	
+	<!-- Contains main navigation, content and footer -->
 	<div id="canvas">
-	<div id="nav_top">
+	
+    <div id="nav_top">
 
-       <div id='nav_links'>             
-
-       <?php include_partial('myModule/navTopLinks'); ?>
-
-         <div id="nav_syslinks_container">
-
-          <div id="nav_langlinks">      
-            <?php
-              $culture = $sf_user->getCulture();
-              $language = substr($culture,0,2);
-              if ($language <> 'en') 
-              {
-                echo ull_link_to('English', 'ullUser/changeCulture?culture=en', 'ull_js_observer_confirm=true');
-              }
-              if ($language <> 'de') 
-              {
-                echo ull_link_to('Deutsch', 'ullUser/changeCulture?culture=de', 'ull_js_observer_confirm=true');
-              }
-            ?>
-          </div>
-          
-          <!-- <div id="nav_searchbox">
-            <?php //include_component('ullWiki', 'headerSearch'); ?>
-          </div> -->
-          
-           <div id="nav_loginbox">
-            <?php include_component('ullUser', 'headerLogin'); ?>
-            </div>
-
-         </div>
-       </div>
-      <div class='clear'></div> <!-- to force the parent-box to enclose the floating divs -->
-     <div id="nav_top_separator_line"></div>     
-     </div>
-     
-     
-     <div id="content">
-        <?php //var_dump(sfContext::getInstance()->getUser()->getAttributeHolder()->getAll()) ?>
-     
-        <?php echo $sf_data->getRaw('sf_content') ?>
+      <div id='nav_links'>             
+        <?php include_partial('myModule/navTopLinks'); ?>
+      </div> 
+      
+      <div id="nav_syslinks_container">
+      
+        <div id="nav_langlinks">      
+          <?php
+            $culture = $sf_user->getCulture();
+            $language = substr($culture,0,2);
+            if ($language <> 'en') 
+            {
+              echo ull_link_to('English', 'ullUser/changeCulture?culture=en', 'ull_js_observer_confirm=true');
+            }
+            if ($language <> 'de') 
+            {
+              echo ull_link_to('Deutsch', 'ullUser/changeCulture?culture=de', 'ull_js_observer_confirm=true');
+            }
+          ?>
+        </div>   
         
-        <?php //phpinfo() ?>
-      </div> <!-- end of content_main -->
-      <div id="footer">
+        <!-- 
+        <div id="nav_searchbox">
+          <?php //include_component('ullWiki', 'headerSearch'); ?>
+        </div> 
+        -->
+          
+        <div id="nav_loginbox">
+          <?php include_component('ullUser', 'headerLogin'); ?>
+        </div>        
+        
+      </div> <!-- end of nav_syslinks_container -->
+      
+      <div id="nav_top_separator_line"></div>      
+   
+      <div class='clear_right'></div><!-- to force the parent-box to enclose the floating divs -->
+            
+    </div> <!-- end of nav_top -->
+     
+     
+    <div id="content">
+      <?php //var_dump(sfContext::getInstance()->getUser()->getAttributeHolder()->getAll()) ?>
+      <?php echo $sf_data->getRaw('sf_content') ?>
+      <?php //phpinfo() ?>
+    </div> <!-- end of content -->
+     
+     
+    <div id="footer">
       <div id="footer_copyright">© 2007-2009 by Klemens Ullmann</div>
       <div id="footer_links">powered by <?php echo ull_link_to('ull.at', 'http://www.ull.at', 'ull_js_observer_confirm=true'); ?></div>
-      <div class='clear'></div> <!-- to force the parent-box to enclose the floating divs -->
-    </div>  
-  </div>
-</div>
+      <!-- <div class='clear'></div> --> 
+    </div>  <!-- end of footer -->
+    
+    
+  </div> <!-- end of canavas -->
+  
+  
+</div> <!--  end of container -->
+
 </body>
+
 </html>
