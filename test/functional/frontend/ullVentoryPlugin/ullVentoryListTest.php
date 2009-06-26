4bdfa72c0c5e2d267039ab8bc55f9fa5342449a6
Klemens Ullmann-Marx <klemens@ull.at>
2009-06-26 07:42:03 +0000

    added ullVentory functional tests for index quicksearch/owner select and create with a given owner

--- test/functional/frontend/ullVentoryPlugin/ullVentoryListTest.php
+++ test/functional/frontend/ullVentoryPlugin/ullVentoryListTest.php
@@ -29,3 +29,58 @@ $b
   ->checkResponseElement($dgsList->get(1, 'model'), 'MacBook')  
   ->checkResponseElement($dgsList->get(2, 'model'), 'MFC-440CN')
 ;
+
+$b
+  ->diag('Index quicksearch')
+  ->get('ullVentory/index')
+  ->setField('filter[search]', '1701') //1701 is an inventory number
+  ->click('Search_16x16')
+  ->isRedirected()
+  ->followRedirect()
+  ->isStatusCode(200)
+  ->isRequestParameter('module', 'ullVentory')
+  ->isRequestParameter('action', 'list')
+  ->isRequestParameter('filter[search]', '1701')
+  ->checkResponseElement($dgsList->getFullRowSelector(), 1) // number of rows
+  ->checkResponseElement($dgsList->get(1, 'inventory_number'), '1701')
+;    
+
+$testUserId = Doctrine::getTable('UllUser')->findOneByUsername('test_user')->id;
+
+$b
+  ->diag('Index owner select')
+  ->get('ullVentory/index')
+  ->setField('filter[ull_entity_id]', $testUserId)
+  ->click('Search_16x16')
+  ->isRedirected()
+  ->followRedirect()
+  ->isStatusCode(200)
+  ->isRequestParameter('module', 'ullVentory')
+  ->isRequestParameter('action', 'list')
+  ->isRequestParameter('filter[ull_entity_id]', $testUserId)
+  ->checkResponseElement('h3', 'Items of Test User')
+  ->checkResponseElement($dgsList->getFullRowSelector(), 2) // number of rows
+  ->checkResponseElement($dgsList->get(1, 'inventory_number'), '1701')
+  ->checkResponseElement($dgsList->get(2, 'inventory_number'), '1702')
+;  
+
+$b
+  ->diag('Now create an entry and check if the owner is passed correctly')
+  ->click('Enlist new item')
+  ->loginAsAdmin()
+  ->isStatusCode(200)
+  ->isRequestParameter('module', 'ullVentory')
+  ->isRequestParameter('action', 'create')
+  ->isRequestParameter('entity', 'test_user')
+  ->setField('fields[type]', 'notebook')
+  ->click('Apply')
+  ->isRedirected()
+  ->followRedirect()
+  ->isStatusCode(200)
+  ->isRequestParameter('module', 'ullVentory')
+  ->isRequestParameter('action', 'createWithType')
+  ->isRequestParameter('entity', 'test_user')
+  ->checkResponseElement('h3', 'Item of Test User')
+;  
+  
+
