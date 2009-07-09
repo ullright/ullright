1c8eb662c54a3fe1f1bfe2d2724fb7a4c5f454e7
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2009-07-09 10:44:26 +0000

    Linked advanced search from ventory index page

--- plugins/ullVentoryPlugin/modules/ullVentory/templates/indexSuccess.php
+++ plugins/ullVentoryPlugin/modules/ullVentory/templates/indexSuccess.php
@@ -14,19 +14,20 @@
     
     <div id="tc_search">
     
-      <?php echo ull_form_tag(array('action' => 'list')); ?>
-        <div class="tc_box_with_bottom_spacer color_medium_bg">
-          <?php echo $form['search']->renderLabel() ?><br />    
-          <?php echo $form['search']->render() ?><?php echo submit_image_tag(ull_image_path('search'), array('class' => 'tc_search_quick_top_img')) ?>
-        </div>
-        
-        <div class="tc_box_with_bottom_spacer color_medium_bg">
-          <?php echo $form['ull_entity_id']->renderLabel() ?><br />
-          <?php echo $form['ull_entity_id']->render() ?><?php echo submit_image_tag(ull_image_path('search'), array('class' => 'tc_search_quick_top_img')) ?>
-        </div>
+    <?php echo ull_form_tag(array('action' => 'list')); ?>
+      <div class="tc_box color_medium_bg">
+        <?php echo $form['search']->renderLabel() ?><br />    
+        <?php echo $form['search']->render() ?><?php echo submit_image_tag(ull_image_path('search'), array('class' => 'tc_search_quick_top_img')) ?>
+      </div>
+      <div class="tc_box_with_bottom_spacer color_light_bg">
+        <?php echo ull_link_to(__('Advanced search', null, 'common'), 'ullVentory/search') ?>
+      </div>
+      <div class="tc_box_with_bottom_spacer color_medium_bg">
+        <?php echo $form['ull_entity_id']->renderLabel() ?><br />
+        <?php echo $form['ull_entity_id']->render() ?><?php echo submit_image_tag(ull_image_path('search'), array('class' => 'tc_search_quick_top_img')) ?>
+      </div>
       </form>
-      
-              
+
       <!-- 
       <div class="tc_search_quick_bottom color_light_bg"><br /><br /><br /><br />tba<br /></div>
       
