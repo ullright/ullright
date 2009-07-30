ed764630a31a88dcf7e36c9b88aaa1fcec7014b1
Klemens Ullmann-Marx <klemens@ull.at>
2009-07-30 20:26:54 +0000

    code style fix

--- plugins/ullCorePlugin/lib/generator/columnConfigCollection/TestTableColumnConfigCollection.class.php
+++ plugins/ullCorePlugin/lib/generator/columnConfigCollection/TestTableColumnConfigCollection.class.php
@@ -10,9 +10,11 @@ class TestTableColumnConfigCollection extends UllEntityColumnConfigCollection
   {
     $this['my_string']->setLabel(__('My custom string label', null, 'ullCoreMessages'));
     $this['my_email']->setMetaWidgetClassName('ullMetaWidgetEmail');
-    $this['my_select_box']->setMetaWidgetClassName('ullMetaWidgetUllSelect')
-    ->setWidgetOption('ull_select', 'ull_select_test')
-    ->setWidgetOption('add_empty', true);
+    $this['my_select_box']
+      ->setMetaWidgetClassName('ullMetaWidgetUllSelect')
+      ->setWidgetOption('ull_select', 'ull_select_test')
+      ->setWidgetOption('add_empty', true)
+    ;
     
     $this->disable(array('my_useless_column'));
   }
