d42aa383a06682b0d4d21a5ab6e2912d7edb7993
Klemens Ullmann-Marx <klemens@ull.at>
2009-08-17 13:41:24 +0000

    ullVentory: added some basic dummy users (migration)

---
+++ plugins/ullCorePlugin/lib/migrations/033_add_ull_ventory_dummy_users.php
@@ -0,0 +1,54 @@
+<?php
+class AddUllVentoryDummyUsers extends Doctrine_Migration
+{
+  public function up()
+  {
+    $x = new UllVentoryOriginDummyUser;
+    $x->username = 'delivered';
+    $x->Translation['en']->display_name = 'Delivered';
+    $x->Translation['de']->display_name = 'Geliefert';
+    $x->save();
+    
+    $x = new UllVentoryOriginDummyUser;
+    $x->username = 'inventory_taking';
+    $x->Translation['en']->display_name = 'Inventory taking';
+    $x->Translation['de']->display_name = 'Inventarisiert';
+    $x->save();    
+    
+    $x = new UllVentoryStatusDummyUser;
+    $x->username = 'stored';
+    $x->Translation['en']->display_name = 'Stored';
+    $x->Translation['de']->display_name = 'Lagernd';
+    $x->save(); 
+      
+    $x = new UllVentoryStatusDummyUser;
+    $x->username = 'disposed';
+    $x->Translation['en']->display_name = 'Disposed';
+    $x->Translation['de']->display_name = 'Entsorgt';
+    $x->save();     
+      
+    $x = new UllVentoryStatusDummyUser;
+    $x->username = 'stolen';
+    $x->Translation['en']->display_name = 'Stolen';
+    $x->Translation['de']->display_name = 'Gestohlen';
+    $x->save();      
+      
+    $x = new UllVentoryStatusDummyUser;
+    $x->username = 'repair';
+    $x->Translation['en']->display_name = 'Repair';
+    $x->Translation['de']->display_name = 'Reparatur';
+    $x->save();
+      
+    $x = new UllVentoryStatusDummyUser;
+    $x->username = 'sold';
+    $x->Translation['en']->display_name = 'Sold';
+    $x->Translation['de']->display_name = 'Verkauft';
+    $x->save();
+        
+    }
+  
+  public function down()
+  {
+    throw new Doctrine_Migration_IrreversibleMigrationException('This migration can not be undone.');
+  }
+}
