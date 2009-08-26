fa1041d29a6b928bdc96054bef5f55ed0eacb284
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2009-08-26 13:30:28 +0000

    Don't try to format an empty string as a number

--- plugins/ullCorePlugin/lib/form/widget/ullWidgetFloatWrite.class.php
+++ plugins/ullCorePlugin/lib/form/widget/ullWidgetFloatWrite.class.php
@@ -4,7 +4,7 @@ class ullWidgetFloatWrite extends ullWidgetFormInput
 {
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
-    if (!(get_class($errors) === 'sfValidatorError'))
+    if (!(get_class($errors) === 'sfValidatorError') && ($value != ''))
     {
       $value = ullMetaWidgetFloat::formatNumber($value);
     }
