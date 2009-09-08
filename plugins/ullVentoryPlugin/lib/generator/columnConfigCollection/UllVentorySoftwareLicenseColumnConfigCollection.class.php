43740dbe4e4ddeaab7f30632a92e508d90e7c1b3
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2009-09-08 06:06:07 +0000

    Added software license translations (#533)

--- plugins/ullVentoryPlugin/lib/generator/columnConfigCollection/UllVentorySoftwareLicenseColumnConfigCollection.class.php
+++ plugins/ullVentoryPlugin/lib/generator/columnConfigCollection/UllVentorySoftwareLicenseColumnConfigCollection.class.php
@@ -12,5 +12,10 @@ class UllVentorySoftwareLicenseColumnConfigCollection extends ullColumnConfigCol
     $this['ull_ventory_software_id']->setLabel(__('Software', null, 'ullVentoryMessages'));
     $this['license_key']->setLabel(__('License key', null, 'ullVentoryMessages'));
     $this['quantity']->setLabel(__('Quantity', null, 'ullVentoryMessages'));
+    $this['supplier']->setLabel(__('Supplier', null, 'ullVentoryMessages'));
+    $this['delivery_date']
+      ->setLabel(__('Delivery date', null, 'ullVentoryMessages'))
+      ->setMetaWidgetClassName('ullMetaWidgetDate')
+    ;
   }
 }
