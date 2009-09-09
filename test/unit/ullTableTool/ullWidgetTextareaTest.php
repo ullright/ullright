bee2a37d44eea234459c0aadf12e460df2e0da95
Klemens Ullmann-Marx <klemens@ull.at>
2009-09-09 13:21:32 +0000

    ullWidgetTextareaTest doesn't need sfContext

--- test/unit/ullTableTool/ullWidgetTextareaTest.php
+++ test/unit/ullTableTool/ullWidgetTextareaTest.php
@@ -5,14 +5,12 @@ require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 sfLoader::loadHelpers(array('Text', 'Tag'));
 
 $t = new lime_test(4, new lime_output_color(), $configuration);
-sfContext::createInstance($configuration);
 
 $w = new ullWidgetTextarea();
 
-// ->render()
 $t->diag('->render()');
-$t->is($w->render('foo', false), "", '->render() renders the widget as HTML');
-$t->is($w->render('foo', 'singleline'), "singleline", '->render() renders the widget as HTML');
-$t->is($w->render('foo', "multi\nline"), "multi<br />\nline", '->render() renders the widget as HTML');
-$t->is($w->render('foo', "vertebratehttp://www.foobar.com fish"),
-      'vertebrate<a href="http://www.foobar.com">http://www.foobar.com</a> fish', '->render() renders the widget as HTML');
+  $t->is($w->render('foo', false), "", '->render() renders the widget as HTML');
+  $t->is($w->render('foo', 'singleline'), "singleline", '->render() renders the widget as HTML');
+  $t->is($w->render('foo', "multi\nline"), "multi<br />\nline", '->render() renders the widget as HTML');
+  $t->is($w->render('foo', "vertebratehttp://www.foobar.com fish"),
+        'vertebrate<a href="http://www.foobar.com">http://www.foobar.com</a> fish', '->render() renders the widget as HTML');
