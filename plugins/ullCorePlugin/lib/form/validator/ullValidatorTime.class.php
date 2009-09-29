ca0bab169a95969eeabd42cd24122c4cee28173d
Klemens Ullmann-Marx <klemens@ull.at>
2009-09-29 13:02:32 +0000

    ullTime: fixed ullTimeValidator empty handling

--- plugins/ullCorePlugin/lib/form/validator/ullValidatorTime.class.php
+++ plugins/ullCorePlugin/lib/form/validator/ullValidatorTime.class.php
@@ -11,6 +11,13 @@
  */
 class ullValidatorTime extends sfValidatorString
 {
+  protected function configure($options = array(), $messages = array())
+  {
+    parent::configure($options, $messages);
+    
+    $this->setOption('empty_value', null);
+  }
+  
   
   protected function doClean($value)
   {
