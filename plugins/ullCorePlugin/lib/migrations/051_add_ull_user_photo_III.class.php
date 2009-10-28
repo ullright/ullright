00627ac017e04ad124a80c3e0cab641244373faa
Klemens Ullmann-Marx <klemens@ull.at>
2009-10-28 09:19:55 +0000

    fixed ullphoto migration

--- plugins/ullCorePlugin/lib/migrations/051_add_ull_user_photo_III.class.php
+++ plugins/ullCorePlugin/lib/migrations/051_add_ull_user_photo_III.class.php
@@ -4,14 +4,12 @@ class AddUllUserPhotoIII extends Doctrine_Migration
 {
   public function up()
   {
-    $this->addColumn('ull_entity_version', 'photo', 'string', array('length' => 255));
     $this->addColumn('ull_ventory_origin_dummy_user_version', 'photo', 'string', array('length' => 255));
     $this->addColumn('ull_ventory_status_dummy_user_version', 'photo', 'string', array('length' => 255));
   }
 
   public function down()
   {
-    $this->removeColumn('ull_entity_version', 'photo');
     $this->removeColumn('ull_ventory_origin_dummy_user_version', 'photo');
     $this->removeColumn('ull_ventory_status_dummy_user_version', 'photo');
   }
