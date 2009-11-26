5b3211b418c19a1b37d20ffd50a8bd9305d3edcb
Klemens Ullmann-Marx <klemens@ull.at>
2009-11-26 08:56:33 +0000

    ullOrgChart: work in progress

---
+++ plugins/ullCorePlugin/lib/ullTree/ullTreeNode.class.php
@@ -0,0 +1,193 @@
+<?php
+
+class ullTreeNode
+{
+  protected 
+    $data,
+    $meta,
+    $subnodes = array()
+  ;
+  
+  /**
+   * Constructor
+   * 
+   * @param mixed $data
+   * @return none
+   */
+  public function __construct($data)
+  {
+    $this->data = $data;  
+  }
+  
+  
+  /**
+   * Get data payload
+   * 
+   * @return mixed
+   */
+  public function getData()
+  {
+    return $this->data;
+  }
+  
+  
+  /**
+   * Add a subnode
+   * 
+   * @param ullTreeNode $node
+   * @return self
+   */
+  public function addSubnode(ullTreeNode $node)
+  {
+    $this->subnodes[] = $node;
+    
+    return $this;
+  }
+  
+  
+  /**
+   * Get the subnodes
+   * 
+   * @return array
+   */
+  public function getSubnodes()
+  {
+    return $this->subnodes;
+  }
+
+  
+  /**
+   * Check if the node has subnodes
+   * 
+   * @return boolean
+   */
+  public function hasSubnodes()
+  {
+    return (count($this->subnodes)) ? true : false;
+  }
+  
+  /**
+   * Set meta data
+   * 
+   * @param string $key
+   * @param mixed $value
+   * @return self
+   */
+  public function setMeta($key, $value)
+  {
+    $this->meta[$key] = $value;
+    
+    return $this;
+  }
+  
+
+  /** 
+   * Check if meta data exists for a given key
+   * 
+   * @param string $key
+   * @return boolean
+   */
+  public function hasMeta($key)
+  {
+    return (isset($this->meta[$key])) ? true : false;
+  }
+  
+  
+  /**
+   * Get meta data
+   * 
+   * @param string $key
+   * @throws InvalidArgumentException
+   * @return mixed
+   */
+  public function getMeta($key)
+  {
+    if (!isset($this->meta[$key]))
+    {
+      throw new InvalidArgumentException('Invalid key given: ' . $key);
+    }
+
+    return $this->meta[$key];
+  }
+  
+  
+  /**
+   * Set if the current node is the leftmost one of a level
+   * 
+   * @param boolean $boolean
+   * @return $this
+   */
+  public function setIsLeftMost($boolean)
+  {
+    $this->setMeta('leftmost', (boolean) $boolean);
+    
+    return $this;
+  }
+  
+  
+  /**
+   * Check if the current node is the leftmost one of a level
+   * @return unknown_type
+   */
+  public function isLeftMost()
+  {
+    if ($this->hasMeta('leftmost'))
+    {
+      return $this->getMeta('leftmost');
+    }
+    
+    return false;
+  }
+  
+  
+  /**
+   * Set if the current node is the leftmost one of a level
+   * 
+   * @param boolean $boolean
+   * @return $this
+   */
+  public function setIsRightMost($boolean)
+  {
+    $this->setMeta('rightmost', (boolean) $boolean);
+    
+    return $this;
+  }
+  
+  
+  /**
+   * Check if the current node is the rightmost one of a level
+   * @return unknown_type
+   */
+  public function isRightMost()
+  {
+    if ($this->hasMeta('rightmost'))
+    {
+      return $this->getMeta('rightmost');
+    }
+    
+    return false;
+  }  
+  
+  
+  /**
+   * Return array format of node and subnodes
+   * 
+   * @return array
+   */
+  public function toArray()
+  {
+    $array = array();
+
+    $array['data'] = $this->data;
+    $array['meta'] = $this->meta;
+    $array['subnodes'] = array();
+    
+    foreach ($this->subnodes as $subnode)
+    {
+      $array['subnodes'][] = $subnode->toArray();
+    }
+    
+    return $array;
+  }
+  
+}
---
+++ test/unit/ullCorePlugin/ullTreeNodeTest.php
@@ -0,0 +1,118 @@
+<?php
+
+include dirname(__FILE__) . '/../../bootstrap/unit.php';
+
+//sfContext::createInstance($configuration);
+//$request = sfContext::getInstance()->getRequest();
+//sfLoader::loadHelpers('ull');
+//sfLoader::loadHelpers('I18N');
+
+$t = new lime_test(18, new lime_output_color);
+
+
+$t->diag('__construct()');
+  $node = new ullTreeNode('testdata');
+  $t->isa_ok($node, 'ullTreeNode', 'Returns the correct object');
+
+  
+$t->diag('getData()');
+  $t->is($node->getData(), 'testdata', 'Returns the correct data');
+  
+  
+$t->diag('addSubnode/getSubnodes()');
+  $node->addSubnode(new ullTreeNode('subnode1 data'));
+
+  $subnodes = $node->getSubnodes();
+  $t->ok(is_array($subnodes), 'Returns an array of subnodes');
+  $t->is(count($subnodes), 1, 'Returns one subnode');
+  $t->is(reset($subnodes)->getData(), 'subnode1 data', 'Subnode contains the correct data');
+  
+  
+$t->diag('hasSubnodes()');
+  $t->ok($node->hasSubnodes(), 'Node has subnodes');
+  $node = new ullTreeNode('testdata');
+  $t->is($node->hasSubnodes(), false, 'Node has no subnodes');
+  
+  
+$t->diag('set/getMeta()');
+  $node->setMeta('leftmost', true);
+  $node->setMeta('something', 'nice');
+  $t->is($node->getMeta('leftmost'), true, 'Returns the correct meta data');
+  $t->is($node->getMeta('something'), 'nice', 'Returns the correct meta data');
+  try
+  {
+    $node->getMeta('invalid');
+    $t->fail('Doesn\'t throw an exception when trying to get an invalid meta data key/value pair');
+  }
+  catch (Exception $e)
+  {
+    $t->pass('Throws an exception when trying to get an invalid meta data key/value pair');
+  }
+  
+  
+$t->diag('hasMeta()');
+  $t->is($node->hasMeta('leftmost'), true, 'Returns true if meta data exist for a given key');
+  $t->is($node->hasMeta('invalid'), false, 'Returns true if meta data exist for a given key');
+  
+  
+$t->diag('isLeftMost()');
+  $node = new ullTreeNode('testdata');
+  $t->is($node->isLeftMost(), false, 'Isn\'t a leftmost node');
+  $node->setIsLeftMost(true);
+  $t->is($node->isLeftMost(), true, 'Is a leftmost node');
+
+  
+$t->diag('isRightMost()');
+  $node = new ullTreeNode('testdata');
+  $t->is($node->isRightMost(), false, 'Isn\'t a rightmost node');
+  $node->setIsRightMost(true);
+  $t->is($node->isRightMost(), true, 'Is a rightmost node');
+  
+  
+$t->diag('toArray()');
+  $node = new ullTreeNode('topnode');
+  
+  $reference = array(
+    'data'     => 'topnode',
+    'meta'     => array(),
+    'subnodes' => array(),
+  );
+  
+  $t->is($node->toArray(), $reference, 'Returns the correct array format for a single node');  
+  
+  $node->addSubnode(new ullTreeNode('subnode1'));
+  $node->addSubnode(new ullTreeNode('subnode2'));
+  $subnode3 = new ullTreeNode('subnode3');
+  $subnode3->addSubnode(new ullTreeNode('subsubnode1'));
+  $node->addSubnode($subnode3);
+  
+  $reference = array(
+    'data'     => 'topnode',
+    'meta'     => array(),
+    'subnodes' => array(
+      0 => array(
+        'data'      => 'subnode1',
+        'meta'      => array(),
+        'subnodes' => array(),
+      ),
+      1 => array(
+        'data'      => 'subnode2',
+        'meta'      => array(),
+        'subnodes' => array(),
+      ),      
+      2 => array(
+        'data'      => 'subnode3',
+        'meta'      => array(),
+        'subnodes' => array(
+        0 => array(
+          'data'      => 'subsubnode1',
+          'meta'      => array(),
+          'subnodes' => array(),
+          ),         
+        ),
+      ),      
+    ),
+  );  
+  
+  $t->is($node->toArray(), $reference, 'Returns the correct array format for a nested node');
+  
