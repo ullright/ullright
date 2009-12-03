fb3dbb278cbfaae8ee68bdb70161e14740b9e4a5
Thomas Strauß <thomas@dekutree.net>
2009-12-03 13:32:17 +0000

    Test commit

--- plugins/ullCorePlugin/config/ullCorePluginConfiguration.class.php
+++ plugins/ullCorePlugin/config/ullCorePluginConfiguration.class.php
@@ -6,7 +6,7 @@
  * @package     ullCore
  * @subpackage  config
  * @author      Your name here
- * @version     SVN: $Id: PluginConfiguration.class.php 12628 2008-11-04 14:43:36Z Kris.Wallsmith $
+ * @version     SVN: $Id$
  * @version     $Rev$
  */
 class ullCorePluginConfiguration extends sfPluginConfiguration
