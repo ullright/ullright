513234fa6910d814999f3199291f7706c7ceffba
Klemens Ullmann-Marx <klemens@ull.at>
2009-12-07 09:50:11 +0000

    ullOrgchart work in progress

--- plugins/ullPhonePlugin/modules/ullOrgchart/templates/listSuccess.php
+++ plugins/ullPhonePlugin/modules/ullOrgchart/templates/listSuccess.php
@@ -4,9 +4,10 @@
 
   <table cellpadding='0' cellspacing='0'>
     <tbody>
-      <tr class='ull_org_chart_boxes_row head'>
-        <td><!-- colspan = number of sub-superiors -->
-          <div class='ull_orgchart_box ull_orgchart_centered'>
+    
+      <tr class='ull_orgchart_head'>
+        <td>
+          <div class='ull_orgchart_box_head'>
             Geschäftsführer
           </div>
         </td>
@@ -14,103 +15,107 @@
   
       <tr class='ull_orgchart_subordinates'>
         <td>
-            <table cellpadding='0' cellspacing='0' class='ull_orgchart_subordinates_table ull_orgchart_centered'>
+          <table cellpadding='0' cellspacing='0'>
 
-              <tr class='ull_orgchart_spacer_row'>
-                <td></td>
-                <td class='ull_orgchart_border_right'>
-                </td>
-                <td class='ull_orgchart_border_left'>
-                </td>
-                <td></td>
-              </tr> 
-              
-              <tr class='ull_orgchart_boxes_row'>
-                <td>
-                  <div class='ull_orgchart_box'>
-                    Assistent1
-                  </div>    
-                </td>
-                <td class='ull_orgchart_spacer_in_between ull_orgchart_border_right'>
-                  <div class='ull_orgchart_border_top'></div>
-                </td>
-                <td class='ull_orgchart_spacer_in_between ull_orgchart_border_left'>
-                    <div class='ull_orgchart_border_top'></div>
-                </td>                
-                <td>
-                  <div class='ull_orgchart_box'>
-                    Assistent2
-                  </div>            
-                </td>
-              </tr>
-              
-              <tr class='ull_orgchart_spacer_row'>
-                <td></td>
-                <td class='ull_orgchart_border_right'>
-                </td>
-                <td class='ull_orgchart_border_left'>
-                </td>
-                <td></td>
-              </tr>              
-                        
-              <tr class='ull_orgchart_boxes_row'>
-                <td>
-                  <div class='ull_orgchart_box'>
-                    Assistent1
-                  </div>    
-                </td>
-                <td class='ull_orgchart_spacer_in_between ull_orgchart_border_right'>
-                  <div class='ull_orgchart_border_top'></div>
-                </td>
-                <td class='ull_orgchart_spacer_in_between ull_orgchart_border_left'>
-                </td>                
-                <td>
-                </td>
-              </tr>      
-              
-              <tr class='ull_orgchart_spacer_row'>
-                <td></td>
-                <td class='ull_orgchart_border_right'>
-                </td>
-                <td class='ull_orgchart_border_left'>
-                </td>
-                <td></td>
-              </tr>                               
-              
-            </table>
+            <tr class='ull_orgchart_spacer_row'>
+              <td>&nbsp;</td>
+              <td class='ull_orgchart_border_right'>&nbsp;</td>
+              <td class='ull_orgchart_border_left'>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr> 
+            
+            <tr class='ull_orgchart_boxes_row'>
+              <td rowspan='2'>
+                <div class='ull_orgchart_box_subordinate'>
+                  Assistent1
+                </div> 
+              </td>
+              <td class='ull_orgchart_border_right ull_orgchart_border_double_bottom'>
+                <!--  necessary to force width for single column subordinates -->
+                <div class='ull_orgchart_spacer_in_between'>&nbsp;</div>
+              </td>
+              <td class='ull_orgchart_border_left ull_orgchart_border_double_bottom'>
+                <div class='ull_orgchart_spacer_in_between'>&nbsp;</div>
+              </td>                
+              <td rowspan='2'>
+                <div class='ull_orgchart_box_subordinate'>
+                  Assistent2
+                </div>            
+              </td>
+            </tr>        
+            <tr>
+              <td class='ull_orgchart_spacer_in_between ull_orgchart_border_right'>&nbsp;</td>
+              <td class='ull_orgchart_spacer_in_between ull_orgchart_border_left'>&nbsp;</td>                
+            </tr> 
+            
+            <tr class='ull_orgchart_spacer_row'>
+              <td>&nbsp;</td>
+              <td class='ull_orgchart_border_right'>&nbsp;</td>
+              <td class='ull_orgchart_border_left'>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>              
+                      
+            <tr class='ull_orgchart_boxes_row'>
+              <td rowspan='2'>
+                <div class='ull_orgchart_box_subordinate'>
+                  Assistent1
+                </div> 
+              </td>
+              <td class='ull_orgchart_border_right ull_orgchart_border_double_bottom'>
+                <div class='ull_orgchart_spacer_in_between'>&nbsp;</div>
+              </td>
+              <td class='ull_orgchart_border_left'>
+                <div class='ull_orgchart_spacer_in_between'>&nbsp;</div>
+              </td>                
+              <td rowspan='2'>&nbsp;</td>
+            </tr>        
+            <tr>
+              <td class='ull_orgchart_spacer_in_between ull_orgchart_border_right'>&nbsp;</td>
+              <td class='ull_orgchart_spacer_in_between ull_orgchart_border_left'>&nbsp;</td>              
+            </tr>    
+            
+            <tr class='ull_orgchart_spacer_row'>
+              <td>&nbsp;</td>
+              <td class='ull_orgchart_border_right'>&nbsp;</td>
+              <td class='ull_orgchart_border_left'>&nbsp;</td>
+              <td>&nbsp;</td>
+            </tr>                               
+            
+          </table>
         </td>
       </tr>
       
+      
       <tr class='ull_orgchart_sub_superiors'>
         <td>
-          <table cellpadding='0' cellspacing='0' class='ull_orgchart_subordinates_table ull_orgchart_centered'>
+          <table cellpadding='0' cellspacing='0'>
           
-            <tr class='ull_orgchart_sub_superiors_spacer_row'>
-              <td class='ull_orgchart_border_right'></td>
-              <td class='ull_orgchart_border_top ull_orgchart_border_left '></td>
-              <td class='ull_orgchart_border_top ull_orgchart_border_right'></td>
-              <td class='ull_orgchart_border_top ull_orgchart_border_left '></td>
-              <td class='ull_orgchart_border_top ull_orgchart_border_right'></td>
-              <td class='ull_orgchart_border_top ull_orgchart_border_left '></td>
-              <td class='ull_orgchart_border_top ull_orgchart_border_right'></td>
-              <td class='ull_orgchart_border_top ull_orgchart_border_left '></td>
-              <td class='ull_orgchart_border_top ull_orgchart_border_right'></td>
-              <td class='ull_orgchart_border_left'></td>
+            <tr class='ull_orgchart_spacer_row'>
+              <td class='ull_orgchart_border_right'>&nbsp;</td>
+              <td class='ull_orgchart_border_top ull_orgchart_border_left '>&nbsp;</td>
+              <td class='ull_orgchart_border_top ull_orgchart_border_right'>&nbsp;</td>
+              <td class='ull_orgchart_border_top ull_orgchart_border_left '>&nbsp;</td>
+              <td class='ull_orgchart_border_top ull_orgchart_border_right'>&nbsp;</td>
+              <td class='ull_orgchart_border_top ull_orgchart_border_left '>&nbsp;</td>
+              <td class='ull_orgchart_border_top ull_orgchart_border_right'>&nbsp;</td>
+              <td class='ull_orgchart_border_top ull_orgchart_border_left '>&nbsp;</td>
+              <td class='ull_orgchart_border_top ull_orgchart_border_right'>&nbsp;</td>
+              <td class='ull_orgchart_border_left'>&nbsp;</td>
             </tr>
             
             <tr class='ull_orgchart_sub_superiors_row'>
               <td colspan="2">
-                <div class='ull_orgchart_box'>
+                <div class='ull_orgchart_box_head'>
                   Head of Marketing
                 </div>               
               </td>
               <td colspan="2">
-                <div class='ull_orgchart_box'>
+                <div class='ull_orgchart_box_head'>
                   Head of IT
                 </div>               
               </td>
               <td colspan="2">
-                <div class='ull_orgchart_box'>
+                <div class='ull_orgchart_box_head'>
                   Head of fools
                 </div>               
               </td>
@@ -118,9 +123,10 @@
               
                 <table cellpadding='0' cellspacing='0'>
                   <tbody>
+                  
                     <tr class='ull_org_chart_boxes_row head'>
-                      <td><!-- colspan = number of sub-superiors -->
-                        <div class='ull_orgchart_box ull_orgchart_centered'>
+                      <td>
+                        <div class='ull_orgchart_box_head'>
                           Head of Superstars
                         </div>
                       </td>
@@ -128,74 +134,63 @@
                 
                     <tr class='ull_orgchart_subordinates'>
                       <td>
-                          <table cellpadding='0' cellspacing='0' class='ull_orgchart_subordinates_table ull_orgchart_centered'>
+                          <table cellpadding='0' cellspacing='0'>
               
                             <tr class='ull_orgchart_spacer_row'>
-                              <td></td>
-                              <td class='ull_orgchart_border_right'>
-                              </td>
-                              <td class='ull_orgchart_border_left'>
-                              </td>
-                              <td></td>
+                              <td>&nbsp;</td>
+                              <td class='ull_orgchart_border_right'>&nbsp;</td>
+                              <td class='ull_orgchart_border_left'>&nbsp;</td>
+                              <td>&nbsp;</td>
                             </tr> 
                             
-                            <!-- unify this with the dual box column layout above --> 
                             <tr class='ull_orgchart_boxes_row'>
                               <td rowspan='2'>
-                                <div class='ull_orgchart_spacer_in_between'></div>
+                                &nbsp;
                               </td>
                               <td class='ull_orgchart_border_right'>
                                 <div class='ull_orgchart_spacer_in_between'>&nbsp;</div>
                               </td>
                               <td class='ull_orgchart_border_left ull_orgchart_border_double_bottom'>
+                                <div class='ull_orgchart_spacer_in_between'>&nbsp;</div>
                               </td>                
                               <td rowspan='2'>
-                                <div class='ull_orgchart_box'>
-                                  Assistent2
+                                <div class='ull_orgchart_box_subordinate'>
+                                  Sub Assistent 1
                                 </div>            
                               </td>
                             </tr>        
-                            <tr class='ull_orgchart_boxes_row'>
-                              <td class='ull_orgchart_border_right'>
-                                <div class='ull_orgchart_spacer_in_between'>&nbsp;</div>
-                              </td>
-                              <td class='ull_orgchart_border_left'>
-                                <div class='ull_orgchart_spacer_in_between'></div>
-                              </td>                
+                            <tr>
+                              <td class='ull_orgchart_spacer_in_between ull_orgchart_border_right'>&nbsp;</td>
+                              <td class='ull_orgchart_spacer_in_between ull_orgchart_border_left'>&nbsp;</td>                
                             </tr>  
                             
                             <tr class='ull_orgchart_spacer_row'>
-                              <td></td>
-                              <td class='ull_orgchart_border_right'>
-                              </td>
-                              <td class='ull_orgchart_border_left'>
-                              </td>
-                              <td></td>
-                            </tr>                             
+                              <td>&nbsp;</td>
+                              <td class='ull_orgchart_border_right'>&nbsp;</td>
+                              <td class='ull_orgchart_border_left'>&nbsp;</td>
+                              <td>&nbsp;</td>
+                            </tr>        
                             
                             <tr class='ull_orgchart_boxes_row'>
                               <td rowspan='2'>
-                                <div class='ull_orgchart_spacer_in_between'></div>
+                                &nbsp;
                               </td>
                               <td class='ull_orgchart_border_right'>
                                 <div class='ull_orgchart_spacer_in_between'>&nbsp;</div>
                               </td>
                               <td class='ull_orgchart_border_left ull_orgchart_border_double_bottom'>
+                                <div class='ull_orgchart_spacer_in_between'>&nbsp;</div>
                               </td>                
                               <td rowspan='2'>
-                                <div class='ull_orgchart_box'>
-                                  Assistent2
+                                <div class='ull_orgchart_box_subordinate'>
+                                  Sub Assistent 2
                                 </div>            
                               </td>
                             </tr>        
-                            <tr class='ull_orgchart_boxes_row'>
-                              <td>
-                                <div class='ull_orgchart_spacer_in_between'>&nbsp;</div>
-                              </td>
-                              <td>
-                                <div class='ull_orgchart_spacer_in_between'></div>
-                              </td>                
-                            </tr>   
+                            <tr>
+                              <td class='ull_orgchart_spacer_in_between'>&nbsp;</td>
+                              <td class='ull_orgchart_spacer_in_between'>&nbsp;</td>                
+                            </tr> 
                             
                           </table>
                       </td>
@@ -206,7 +201,7 @@
                 </table>              
               </td>
               <td colspan="2">
-                <div class='ull_orgchart_box'>
+                <div class='ull_orgchart_box_head'>
                   Head of Oranges
                 </div>               
               </td>
