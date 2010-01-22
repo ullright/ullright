c85da49f63592a0aa2108081eefae9abc183166d
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2010-01-22 11:21:53 +0000

    Deleted old migrations and fixed auto-insertion of current migration version

--- plugins/ullCorePlugin/lib/migrations/1263823760_ull_wiki_deleted_at.php
+++ plugins/ullCorePlugin/lib/migrations/1263823760_ull_wiki_deleted_at.php
@@ -4,12 +4,16 @@ class UllWikiDeletedAt extends Doctrine_Migration_Base
 {
   public function up()
   {
+    //TODO: Does this work? What happens to data/non-timestamps?
     $this->renameColumn('ull_wiki', 'deleted', 'deleted_at');
     $this->changeColumn('ull_wiki', 'deleted_at', 'timestamp');
   }
 
   public function down()
   {
-    throw new Doctrine_Migration_IrreversibleMigrationException('This migration can not be undone.'); 
+    //TODO: Does this work? What happens to data/non-booleans?
+    //Throw Exception if we can't make it work
+    $this->changeColumn('ull_wiki', 'deleted_at', 'boolean');
+    $this->renameColumn('ull_wiki', 'deleted_at', 'deleted');
   }
 }
