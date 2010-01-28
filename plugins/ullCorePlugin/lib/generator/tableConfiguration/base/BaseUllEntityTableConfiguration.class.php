214c6473683509f59abe3189726f87042357c1d1
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2010-01-28 14:28:54 +0000

    Sidebar CSS print fix

---
+++ plugins/ullCorePlugin/lib/generator/tableConfiguration/base/BaseUllEntityTableConfiguration.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * TableConfiguration for UllUser
+ * 
+ * @author klemens.ullmann-marx@ull.at
+ *
+ */
+class BaseUllEntityTableConfiguration extends ullTableConfiguration
+{
+  /**
+   * (non-PHPdoc)
+   * @see plugins/ullCorePlugin/lib/ullTableConfiguration#applyCustomSettings()
+   */
+  protected function applyCustomSettings()
+  {
+    $this
+      ->setName(__('Users', null, 'ullCoreMessages'))
+      ->setSearchColumns(array('display_name', 'username', 'email', 'UllLocation->name', 'UllDepartment->name'))
+      ->setOrderBy('last_name, first_name')
+      ->setListColumns(array('id', 'first_name', 'last_name', 'username', 'email', 'UllLocation->name', 'UllDepartment->name'))
+      // We don't need to set the foreign relation names here, because ullHumanizer does the job
+//      ->setForeignRelationName(__('User', null, 'ullCoreMessages'))
+    ;
+  }
+  
+}
