b154950f0d6d06d9895c394e648d0443acb52b7c
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2010-02-04 14:47:30 +0000

    Updated and enhanced test

--- test/unit/ullTimePlugin/UllTimePeriodTest.php
+++ test/unit/ullTimePlugin/UllTimePeriodTest.php
@@ -9,7 +9,7 @@ class myTestCase extends sfDoctrineTestCase
 // create context since it is required by ->getUser() etc.
 sfContext::createInstance($configuration);
 
-$t = new myTestCase(5, new lime_output_color, $configuration);
+$t = new myTestCase(6, new lime_output_color, $configuration);
 $path = dirname(__FILE__);
 $t->setFixturesPath($path);
 
@@ -19,7 +19,8 @@ $t->begin('getDateList()');
   $list = $period->getDateList();
   $t->is(is_array($list), true, 'returns an array');
   $t->is(count($list), 30, 'returns the correct number of days');
-  $t->is($list['2009-09-01'], array('date' => '2009-09-01', 'weekend' => false), 'returns the correct entry for the first day');
-  $t->is($list['2009-09-05'], array('date' => '2009-09-05', 'weekend' => true), 'returns the correct entry for the first day');
-  $t->is($list['2009-09-06'], array('date' => '2009-09-06', 'weekend' => true), 'returns the correct entry for the first day');
+  $t->is($list['2009-09-01'], array('date' => '2009-09-01', 'weekend' => false, 'calendarWeek' => 36), 'returns the correct entry for the first day');
+  $t->is($list['2009-09-05'], array('date' => '2009-09-05', 'weekend' => true, 'calendarWeek' => 36), 'returns the correct entry for the first day');
+  $t->is($list['2009-09-06'], array('date' => '2009-09-06', 'weekend' => true, 'calendarWeek' => 36), 'returns the correct entry for the first day');
+  $t->is($list['2009-09-16'], array('date' => '2009-09-16', 'weekend' => false, 'calendarWeek' => 38), 'returns the correct entry for the first day');
   
