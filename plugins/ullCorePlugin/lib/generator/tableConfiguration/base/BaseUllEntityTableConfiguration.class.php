ce99fe98a5d4c7a912478fa9c46461a795bbf286
Klemens Ullmann-Marx <klemens@ull.at>
2010-02-15 19:25:47 +0000

    migration to sf13 & phone book updates

--- plugins/ullCorePlugin/lib/generator/tableConfiguration/base/BaseUllEntityTableConfiguration.class.php
+++ plugins/ullCorePlugin/lib/generator/tableConfiguration/base/BaseUllEntityTableConfiguration.class.php
@@ -15,7 +15,7 @@ class BaseUllEntityTableConfiguration extends ullTableConfiguration
   {
     $this
       ->setName(__('Users', null, 'ullCoreMessages'))
-      ->setSearchColumns(array('display_name', 'username', 'email', 'UllLocation->name', 'UllDepartment->name'))
+      ->setSearchColumns(array('id', 'display_name', 'username', 'email', 'UllLocation->name', 'UllDepartment->name'))
       ->setOrderBy('last_name, first_name')
       ->setListColumns(array('id', 'first_name', 'last_name', 'username', 'email', 'UllLocation->name', 'UllDepartment->name'))
       // We don't need to set the foreign relation names here, because ullHumanizer does the job
