0ea2edc644a1e7475c33f7b68fbd3de4f6aace45
Thomas Strauß <thomas@dekutree.net>
2010-03-03 09:44:22 +0000

    added ullUserPopupExists test

---
+++ test/functional/frontend/ullCorePlugin/ullUserPopupExistsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+include dirname(__FILE__) . '/../../../bootstrap/functional.php';
+
+$b = new ullTestBrowser(null, null, array('configuration' => $configuration));
+$path = dirname(__FILE__);
+$b->setFixturesPath($path);
+$b->resetDatabase();
+
+$b
+  ->diag('ullPhone Home')
+  ->get('ullAdmin/index')
+  ->loginAsAdmin()
+  ->get('ullPhone/list')
+  ->isStatusCode(200)
+  ->with('request')->begin()
+    ->isParameter('module', 'ullPhone')
+    ->isParameter('action', 'list')
+  ->end()
+;
+
+$b
+  ->diag('ullUser: open UserPopup')
+  ->click('User Test')
+  ->isStatusCode(200)
+  ->with('request')->begin()
+    ->isParameter('module', 'ullUser')
+    ->isParameter('action', 'show')
+    ->isParameter('username', Doctrine::getTable('UllUser')->findOneByUserName('test_user')->id)
+  ->end()
+  ->with('response')->begin()
+    ->contains('test_user')
+    ->contains('Superior')
+  ->end()
+;
