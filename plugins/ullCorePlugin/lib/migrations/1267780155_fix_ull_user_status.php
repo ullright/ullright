16c91c0b867b0cbddd2e42282d23fa512d3087fe
Klemens Ullmann-Marx <klemens@ull.at>
2010-03-05 09:31:59 +0000

    Fixed migration

--- plugins/ullCorePlugin/lib/migrations/1267780155_fix_ull_user_status.php
+++ plugins/ullCorePlugin/lib/migrations/1267780155_fix_ull_user_status.php
@@ -9,9 +9,9 @@ class FixUllUserStatus extends Doctrine_Migration_Base
   public function postUp()
   {
     $dbh = Doctrine_Manager::getInstance()->getCurrentConnection()->getDbh();
-    $dbh->exec("UPDATE ull_user_status SET slug='inactive' WHERE slug='separated");
-    $dbh->exec("UPDATE ull_user_status_translation SET name='Inactive' WHERE name='Separated");
-    $dbh->exec("UPDATE ull_user_status_translation SET name='Inaktiv' WHERE name='Ausgetreten");
+    $dbh->exec("UPDATE ull_user_status SET slug='inactive' WHERE slug='separated'");
+    $dbh->exec("UPDATE ull_user_status_translation SET name='Inactive' WHERE name='Separated'");
+    $dbh->exec("UPDATE ull_user_status_translation SET name='Inaktiv' WHERE name='Ausgetreten'");
   }
 
   public function down()
