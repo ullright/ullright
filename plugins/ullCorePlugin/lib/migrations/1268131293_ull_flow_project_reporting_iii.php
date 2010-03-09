be2ea47be78e902f59ee866f578289fe64fc5b4a
Klemens Ullmann-Marx <klemens@ull.at>
2010-03-09 10:43:29 +0000

    typo

--- plugins/ullCorePlugin/lib/migrations/1268131293_ull_flow_project_reporting_iii.php
+++ plugins/ullCorePlugin/lib/migrations/1268131293_ull_flow_project_reporting_iii.php
@@ -4,8 +4,8 @@ class UllFlowProjectReportingIii extends Doctrine_Migration_Base
 {
   public function up()
   {
-    $this->removeColumn('ull_time_reporting');
-    $this->removeColumn('ull_time_reporting');
+    $this->removeColumn('ull_time_reporting', 'linked_model');
+    $this->removeColumn('ull_time_reporting', 'linked_id');
     
     $this->addColumn('ull_project_reporting', 'linked_model', 'string', 128);
     $this->addColumn('ull_project_reporting', 'linked_id', 'integer');    
