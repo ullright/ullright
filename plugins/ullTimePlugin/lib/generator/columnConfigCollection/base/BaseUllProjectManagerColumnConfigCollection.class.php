8a69ce4c5c310ab544cd762e3270e67c04790ae3
Klemens Ullmann-Marx <klemens@ull.at>
2010-03-10 11:35:28 +0000

    ullTime: admin -> managers: add search boxes

--- plugins/ullTimePlugin/lib/generator/columnConfigCollection/base/BaseUllProjectManagerColumnConfigCollection.class.php
+++ plugins/ullTimePlugin/lib/generator/columnConfigCollection/base/BaseUllProjectManagerColumnConfigCollection.class.php
@@ -10,12 +10,14 @@ class BaseUllProjectManagerColumnConfigCollection extends ullColumnConfigCollect
   protected function applyCustomSettings()
   {
     $this['ull_user_id']
-      ->setWidgetOption('add_empty', true);
+      ->setWidgetOption('add_empty', true)
+      ->setWidgetOption('show_search_box', true)
     ;
     
     $this['ull_project_id']
       ->setLabel(__('Project', null, 'ullTimeMessages'))
-      ->setWidgetOption('add_empty', true);
+      ->setWidgetOption('add_empty', true)
+      ->setWidgetOption('show_search_box', true)
     ;
     
     $this->order(array(
