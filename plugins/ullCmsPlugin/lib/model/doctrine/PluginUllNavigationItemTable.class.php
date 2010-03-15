3fbb53b9da0b3419109c54de8f42e8d36db65363
Klemens Ullmann-Marx <klemens@ull.at>
2010-03-15 09:48:29 +0000

    ullCms progress

--- plugins/ullCmsPlugin/lib/model/doctrine/PluginUllNavigationItemTable.class.php
+++ plugins/ullCmsPlugin/lib/model/doctrine/PluginUllNavigationItemTable.class.php
@@ -3,5 +3,19 @@
  */
 class PluginUllNavigationItemTable extends UllRecordTable
 {
+  
+  
+  public static function getNavigationArray($parentSlug = null, $currentSlug = null)
+  {
+    $q = new Doctrine_Query;
+    $q
+      ->from('UllNavigationItem n')
+      ->where('n.slug = ?', 'main-navigation')
+    ;
+    
+    $result = $q->execute(array(), DOCTRINE::HYDRATE_ARRAY);
+    
+    var_dump($result);
+  }
 
 }
