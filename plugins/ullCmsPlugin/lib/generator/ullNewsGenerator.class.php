f9a0e4a75235362c894ce509138dcd5e4c0236a0
Thomas Strauß <thomas@dekutree.net>
2010-03-16 13:30:25 +0000

    created ullNews module

---
+++ plugins/ullCmsPlugin/lib/generator/ullNewsGenerator.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class ullNewsGenerator extends ullTableToolGenerator
+{
+
+  public function __construct($defaultAccess = null, $requestAction = null, $columns = array())
+  {
+    parent::__construct('UllNews', $defaultAccess, $requestAction, $columns);
+  }
+  
+  
+//  public function configure()
+//  {
+//    $this->setAllowDelete(false);
+//  }
+  
+}
