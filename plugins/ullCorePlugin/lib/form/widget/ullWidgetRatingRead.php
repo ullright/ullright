14e374f16e9f6d53cdeb82416469216822585c4a
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2010-04-13 12:36:49 +0000

    ullWidgetRatingRead: enhanced name attribute creation; added support files

--- plugins/ullCorePlugin/lib/form/widget/ullWidgetRatingRead.php
+++ plugins/ullCorePlugin/lib/form/widget/ullWidgetRatingRead.php
@@ -1,6 +1,13 @@
 <?php
 class ullWidgetRatingRead extends ullWidget
 {
+ public function __construct($options = array(), $attributes = array())
+  {
+    //push this option into superclass?
+    $this->addOption('add_random_identifier', true);
+    parent::__construct($options, $attributes);
+  }
+  
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
     if (empty($value))
@@ -12,21 +19,25 @@ class ullWidgetRatingRead extends ullWidget
     $roundedAvg = round($value, 1);
     $checkedStar = $roundedAvg / 0.5;
     
-    if (!$this->getAttribute('name'))
-    {
-      $this->setAttribute('name', $name);
-    }
-    
-    $randId = uniqid();
+    $name = ($name) ? $name : 'avg_rating';
+    $this->setAttribute('name', $this->getOption('add_random_identifier') ? $name . '_' . uniqid() : $name);
+
+    $attributeArray = array(
+      'class' => 'star {split:2}',
+      'type' => 'radio',
+      'disabled' => 'disabled'
+    );
     
     for($i = 1; $i <= 10; $i++)
     {
-      $html .= '<input class="star {split:2}" type="radio" name="avg_rating_' . $randId . '" disabled="disabled" ';
       if ($i == $checkedStar)
       {
-        $html .= 'checked="checked" ';
+        $html .= $this->renderTag('input', $attributeArray + array('checked' => 'checked'));
       }
-      $html .= '/>';
+      else
+      {
+        $html .= $this->renderTag('input', $attributeArray);
+      }
     }
 
     return $html;
