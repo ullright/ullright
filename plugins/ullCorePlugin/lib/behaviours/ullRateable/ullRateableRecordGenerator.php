ff76c4361dede2681e174607f320a36f75a082fe
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2010-04-13 10:55:12 +0000

    ullRateable: code style

--- plugins/ullCorePlugin/lib/behaviours/ullRateable/ullRateableRecordGenerator.php
+++ plugins/ullCorePlugin/lib/behaviours/ullRateable/ullRateableRecordGenerator.php
@@ -43,25 +43,25 @@ class ullRateableRecordGenerator extends Doctrine_Record_Generator
     return $result['average_rating'];
   }
 
-  public function getUserRating(Doctrine_Record $invoker, $ull_user_id)
+  public function getUserRating(Doctrine_Record $invoker, $ullUserId)
   {
     $q = Doctrine_Query::create()
       ->select('ratings.rating')
       ->from($this->getOption('className') . ' as ratings')
       ->where('ratings.id = ?', $invoker->id)
-      ->andWhere('ratings.voter_ull_user_id = ?', $ull_user_id)
+      ->andWhere('ratings.voter_ull_user_id = ?', $ullUserId)
     ;
     
     $result = $q->fetchOne();
     return $result['rating'];
   }
   
-  public function setUserRating(Doctrine_Record $invoker, $ull_user_id, $rating)
+  public function setUserRating(Doctrine_Record $invoker, $ullUserId, $rating)
   {
     $className = $this->getOption('className');
     $ratingObject = new $className();
 
-    $ratingObject['voter_ull_user_id'] = $ull_user_id;
+    $ratingObject['voter_ull_user_id'] = $ullUserId;
     $ratingObject['id'] = $invoker->id;
 
     if ($rating)
@@ -73,11 +73,10 @@ class ullRateableRecordGenerator extends Doctrine_Record_Generator
     {
       //why doesn't this work?
       //$ratingObject->delete();
-      
       $q = Doctrine_Query::create()
         ->delete($this->getOption('className') . ' as ratings')
         ->where('ratings.id = ?', $invoker->id)
-        ->andWhere('ratings.voter_ull_user_id = ?', $ull_user_id)
+        ->andWhere('ratings.voter_ull_user_id = ?', $ullUserId)
       ;
       
       $q->execute();
