379971079f4cca9d4ce9cc902b2a6d6c025c4aba
Thomas Strauß <thomas@dekutree.net>
2010-04-22 10:49:38 +0000

    ullNews: made image link-able

--- plugins/ullCmsPlugin/modules/ullNews/templates/_newsList.php
+++ plugins/ullCmsPlugin/modules/ullNews/templates/_newsList.php
@@ -3,17 +3,22 @@
     <?php foreach ($generator->getForms() as $row => $form): ?>
       <div class="ull_news_entry">
         <div class="ull_news_image">
-          <?php echo $form['image_upload']->render() ?>
+          <?php if ($form['link_url']->render() && $form['image_upload']->render()): ?>
+            <?php echo link_to($form['image_upload']->render(), 
+              $form['link_url']->render()) ?>
+          <?php else: ?>
+            <?php echo $form['image_upload']->render() ?>
+          <?php endif ?>
         </div>
         <div class="ull_news_content">
           <div class="ull_news_date"><?php echo $form['activation_date']->render() ?></div>
           <h3 class="ull_news_title"><?php echo $form['title']->render() ?></h3>
           <p class="ull_news_abstract"><?php echo $form['abstract']->render() ?></p>
           <?php if ($form['link_name']->render() && $form['link_url']->render()): ?>
-          	<p class="ull_news_link">
-          	 <?php echo link_to($form['link_name']->render(), 
-          	   $form['link_url']->render()) ?>
-          	</p>
+            <p class="ull_news_link">
+              <?php echo link_to($form['link_name']->render(), 
+               $form['link_url']->render()) ?>
+            </p>
           <?php endif ?>
         </div>
       </div>
