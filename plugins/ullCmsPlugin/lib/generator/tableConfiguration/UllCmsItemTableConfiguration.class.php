4bee0c223e72d6670e74498822b543d9b9da693a
Klemens Ullmann-Marx <klemens@ull.at>
2010-04-22 08:10:09 +0000

    ullNews: admin UllCmsItem: default is_active filter to null

--- plugins/ullCmsPlugin/lib/generator/tableConfiguration/UllCmsItemTableConfiguration.class.php
+++ plugins/ullCmsPlugin/lib/generator/tableConfiguration/UllCmsItemTableConfiguration.class.php
@@ -15,7 +15,7 @@ class UllCmsItemTableConfiguration extends ullTableConfiguration
   {
     $this->setForeignRelationName(__('Higher menu entry', null, 'ullCmsMessages'));
     $this->setForeignRelationName(__('Higher', null, 'ullCmsMessages'), 'Parent');
-    $this->setFilterColumns(array('is_active' => 'checked'));
+    $this->setFilterColumns(array('is_active' => null));
   }
   
 }
