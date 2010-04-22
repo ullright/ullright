795022420f2d915b38fdefb2d27fcc9a9ebd4773
Klemens Ullmann-Marx <klemens@ull.at>
2010-04-22 07:28:57 +0000

    Fixed formatting

--- plugins/ullCmsPlugin/modules/ullNews/templates/newsListFeedSuccess.php
+++ plugins/ullCmsPlugin/modules/ullNews/templates/newsListFeedSuccess.php
@@ -7,23 +7,23 @@
     <name><?php echo sfConfig::get('app_ull_news_rss_author') ?></name>
   </author>
   <title><?php echo __(sfConfig::get('app_ull_news_rss_title', 'ullright newsfeed')) ?></title>
- 	<id>urn:uuid:<?php echo $uuid ?></id>
- 	<logo><?php echo sfConfig::get('app_ull_news_rss_logo') ?></logo>
+  <id>urn:uuid:<?php echo $uuid ?></id>
+  <logo><?php echo sfConfig::get('app_ull_news_rss_logo') ?></logo>
   <updated><?php echo gmstrftime('%Y-%m-%dT%H:%M:%SZ', Doctrine::getTable('UllNews')->findLatestNews()->getDateTimeObject('activation_date')->format('U')) ?></updated>
 
-  <?php foreach($newsEntries as $newsEntry):?>
-  	<entry>
-		<title><?php echo $newsEntry['title'] ?></title>
-		<link href="<?php echo url_for('@homepage', true) ?>"/>
-  	<id>urn:uuid:<?php echo sha1($newsEntry['slug'] . $newsEntry['created_at'] . $uuid) ?></id>
-  	<updated><?php echo gmstrftime('%Y-%m-%dT%H:%M:%SZ', Doctrine::getTable('UllNews')->findOneBySlug($newsEntry['slug'])->getDateTimeObject('activation_date')->format('U')) ?></updated>
+  <?php foreach($newsEntries as $newsEntry): ?>
+  <entry>
+    <title><?php echo $newsEntry['title'] ?></title>
+    <link href="<?php echo url_for('@homepage', true) ?>"/>
+    <id>urn:uuid:<?php echo sha1($newsEntry['slug'] . $newsEntry['created_at'] . $uuid) ?></id>
+    <updated><?php echo gmstrftime('%Y-%m-%dT%H:%M:%SZ', Doctrine::getTable('UllNews')->findOneBySlug($newsEntry['slug'])->getDateTimeObject('activation_date')->format('U')) ?></updated>
     <summary type="html"><![CDATA[
       <p><?php echo nl2br($newsEntry['abstract']) ?></p>
       <?php if ($newsEntry['link_name'] && $newsEntry['link_url']): ?>
-        <p><?php echo link_to($newsEntry['link_name'], $newsEntry['link_url'], array('absolute' => true)) ?></p> 
-    <?php endif ?> ]]>
+      <p><?php echo link_to($newsEntry['link_name'], $newsEntry['link_url'], array('absolute' => true)) ?></p> 
+      <?php endif ?> ]]>
     </summary>
-  </entry>
+    </entry>
+    
   <?php endforeach ?>
 </feed>
-
