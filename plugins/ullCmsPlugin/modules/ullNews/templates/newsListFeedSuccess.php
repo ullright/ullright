09d20c8ee5d39d747b2c4167d02c1a91afaa714a
Thomas Strauß <thomas@dekutree.net>
2010-04-28 10:33:29 +0000

    ullNews: refactored feed

--- plugins/ullCmsPlugin/modules/ullNews/templates/newsListFeedSuccess.php
+++ plugins/ullCmsPlugin/modules/ullNews/templates/newsListFeedSuccess.php
@@ -1,13 +1,12 @@
 <?php echo '<?xml version="1.0" encoding="utf-8"?>' ?>
 
-<?php $uuid = sha1(sfConfig::get('app_ull_news_rss_title', 'ullright newsfeed') . url_for('@homepage', true))?>
-
 <feed xmlns="http://www.w3.org/2005/Atom">
   <author>
     <name><?php echo sfConfig::get('app_ull_news_rss_author') ?></name>
   </author>
   <title><?php echo __(sfConfig::get('app_ull_news_rss_title', 'ullright newsfeed')) ?></title>
-  <id>urn:uuid:<?php echo $uuid ?></id>
+  <link rel="self" href="<?php echo $sf_request->getUri()?>" />
+  <id><?php echo url_for('@homepage', true). 'feed' ?></id>
   <logo><?php echo sfConfig::get('app_ull_news_rss_logo') ?></logo>
   <updated><?php echo gmstrftime('%Y-%m-%dT%H:%M:%SZ', Doctrine::getTable('UllNews')->findLatestNews()->getDateTimeObject('activation_date')->format('U')) ?></updated>
 
@@ -15,7 +14,7 @@
   <entry>
     <title><?php echo $newsEntry['title'] ?></title>
     <link href="<?php echo url_for('@homepage', true) ?>"/>
-    <id>urn:uuid:<?php echo sha1($newsEntry['slug'] . $newsEntry['created_at'] . $uuid) ?></id>
+    <id><?php echo url_for('@homepage', true) . 'feed/' . $newsEntry['slug'] ?></id>
     <updated><?php echo gmstrftime('%Y-%m-%dT%H:%M:%SZ', Doctrine::getTable('UllNews')->findOneBySlug($newsEntry['slug'])->getDateTimeObject('activation_date')->format('U')) ?></updated>
     <summary type="html"><![CDATA[
       <p><?php echo nl2br($newsEntry['abstract']) ?></p>
