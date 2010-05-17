14a2fbd2ab76c4e76692ec48b22d724873a68624
Klemens Ullmann-Marx <klemens@ull.at>
2010-05-17 08:59:04 +0000

    Added config option if users are allowed to edit their account (header partial)

--- plugins/ullCorePlugin/modules/ullUser/templates/_headerSyslinkMyAccount.php
+++ plugins/ullCorePlugin/modules/ullUser/templates/_headerSyslinkMyAccount.php
@@ -1,11 +1,15 @@
-<?php if (sfConfig::get('app_ull_user_enable_sign_up', false)): ?>
+<?php // checking for $username means check if logged in ?>
+
+<?php if (sfConfig::get('app_ull_user_enable_sign_up', false) && !$username): ?>
   <li>
-    <?php if ($username): // check if logged in ?>
-      <?php echo link_to(__('My account', null, 'ullCoreMessages'), 
-        'ullUser/editAccount?username=' . $username) ?>
-    <?php else: ?>
-      <?php echo link_to(__('Sign up', null, 'ullCoreMessages'), 
-        'ullUser/signUp')?>  
-    <?php endif ?>
+    <?php echo link_to(__('Sign up', null, 'ullCoreMessages'), 
+      'ullUser/signUp')?>  
+  </li>
+<?php endif ?>
+
+<?php if (sfConfig::get('app_ull_user_enable_my_account', false) && $username): ?>
+  <li>
+    <?php echo link_to(__('My account', null, 'ullCoreMessages'), 
+      'ullUser/editAccount?username=' . $username) ?>
   </li>
 <?php endif ?>
