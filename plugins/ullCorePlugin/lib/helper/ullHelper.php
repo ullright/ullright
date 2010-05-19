0acae6816ab3b49e01ceede14ed786651a15b3e2
Klemens Ullmann-Marx <klemens@ull.at>
2010-05-19 12:31:58 +0000

    Changed QR code provider

--- plugins/ullCorePlugin/lib/helper/ullHelper.php
+++ plugins/ullCorePlugin/lib/helper/ullHelper.php
@@ -1188,7 +1188,8 @@ function link_to_google_maps($link, $searchString, $options)
  */
 function ull_url_for_google_qrcode($content, $size = '120x120')
 {
-  return 'http://chart.apis.google.com/chart?cht=qr&chs=' . $size . '&chl=' . urlencode($content) . '&chld=Q';
+//  return 'http://chart.apis.google.com/chart?cht=qr&chs=' . $size . '&chl=' . urlencode($content) . '&chld=Q|0';
+  return 'http://api.qrserver.com/v1/create-qr-code/?data=' . urlencode($content) . '&size=' . $size . '&margin=0&ecc=Q';
 }
 
 /**
