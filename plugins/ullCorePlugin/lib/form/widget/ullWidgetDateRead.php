cd1b9c69b45a2ea1eccdf18634cea04cd244133b
Klemens Ullmann-Marx <klemens@ull.at>
2010-06-14 19:18:37 +0000

    Added "show_only_year" option to ullWidgetDateRead; Added "add_icon_th" option to ullResultListHeaderNoOrder partial

--- plugins/ullCorePlugin/lib/form/widget/ullWidgetDateRead.php
+++ plugins/ullCorePlugin/lib/form/widget/ullWidgetDateRead.php
@@ -4,7 +4,9 @@ class ullWidgetDateRead extends ullWidget
 {
   public function __construct($options = array(), $attributes = array())
   {
-    $this->addOption('show_weekday');
+    $this->addOption('show_weekday', false);
+    $this->addOption('show_only_year', false);
+    
     parent::__construct($options, $attributes);
   }
   
@@ -12,7 +14,21 @@ class ullWidgetDateRead extends ullWidget
   {
     //ull_format_date renders the current date when $value is NULL
     //but we actually need an empty field
-    return ($value) ? ull_format_date($value, true, $this->getOption('show_weekday')) : '';
-    //return ull_format_date($value);
+    
+    if (!$value)
+    {
+      return '';
+    }
+    
+    if ($this->getOption('show_only_year'))
+    {
+      $value = date('Y', strtotime($value));
+    }
+    else
+    {
+      $value = ull_format_date($value, true, $this->getOption('show_weekday'));
+    }
+    
+    return $value;
   } 
 }
--- plugins/ullCorePlugin/modules/ullTableTool/templates/_ullResultListHeaderNoOrder.php
+++ plugins/ullCorePlugin/modules/ullTableTool/templates/_ullResultListHeaderNoOrder.php
@@ -1,5 +1,10 @@
   <tr>  
-    <th class="color_dark_bg">&nbsp;</th>
+
+    <?php $renderIconHeader = !(isset($add_icon_th) && $add_icon_th == false) ? true : false ?>
+  
+    <?php if ($renderIconHeader): ?>
+      <th class="color_dark_bg">&nbsp;</th>
+    <?php endif ?>
     
     <?php foreach ($generator->getAutoRenderedLabels() as $field_name => $label): ?>
       <th class="color_dark_bg">
