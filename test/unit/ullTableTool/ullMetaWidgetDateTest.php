273067d35c880669c1760b4f452d8c7643a2ea43
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2010-07-01 13:40:32 +0000

    ullMetaWidgetDateTest: fixed

--- test/unit/ullTableTool/ullMetaWidgetDateTest.php
+++ test/unit/ullTableTool/ullMetaWidgetDateTest.php
@@ -36,4 +36,4 @@ $t->diag('for write access:');
   $t->isa_ok($widget, 'ullMetaWidgetDate', '__construct() returns the correct object');
   $widget->addToFormAs('my_field');
   $t->isa_ok($form->getWidgetSchema()->offsetGet('my_field'), 'ullWidgetDateWrite', 'returns the correct widget for write access');
-  $t->isa_ok($form->getValidatorSchema()->offsetGet('my_field'), 'sfValidatorDate', 'returns the correct validator for write access');
+  $t->isa_ok($form->getValidatorSchema()->offsetGet('my_field'), 'ullValidatorDate', 'returns the correct validator for write access');
