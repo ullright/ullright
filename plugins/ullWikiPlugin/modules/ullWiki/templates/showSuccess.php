bd3925279dd8ffbed2b0f74630cfba4b4d000872
Klemens Ullmann-Marx <klemens@ull.at>
2010-07-02 07:10:30 +0000

    Cleaned up wiki show action

--- plugins/ullWikiPlugin/modules/ullWiki/templates/showSuccess.php
+++ plugins/ullWikiPlugin/modules/ullWiki/templates/showSuccess.php
@@ -1,11 +1,11 @@
-<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/src/shCore.js')?>
-<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/scripts/shBrushPhp.js')?>
-<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/scripts/shBrushBash.js')?>
-<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/scripts/shBrushCss.js')?>
-<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/scripts/shBrushSql.js')?>
-<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/scripts/shBrushXml.js')?>
-<?php echo use_stylesheet('/ullCorePlugin/js/syntaxhighlighter/styles/shCore.css')?>
-<?php echo use_stylesheet('/ullCorePlugin/js/syntaxhighlighter/styles/shThemeDefault.css')?>
+<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/src/shCore.js') ?>
+<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/scripts/shBrushPhp.js') ?>
+<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/scripts/shBrushBash.js') ?>
+<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/scripts/shBrushCss.js') ?>
+<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/scripts/shBrushSql.js') ?>
+<?php echo use_javascript('/ullCorePlugin/js/syntaxhighlighter/scripts/shBrushXml.js') ?>
+<?php echo use_stylesheet('/ullCorePlugin/js/syntaxhighlighter/styles/shCore.css') ?>
+<?php echo use_stylesheet('/ullCorePlugin/js/syntaxhighlighter/styles/shThemeDefault.css') ?>
 
 
 <?php echo $breadcrumb_tree ?>
@@ -19,117 +19,33 @@
 <?php endif ?>  
 
 <div id="ull_wiki_header">
-<?php include_component(
-        'ullWiki', 
-        'ullWikiHeaderShow', 
-        array('doc' => $doc, 'cursor' => 0)
-      ); ?>
+  <?php include_component(
+    'ullWiki', 
+    'ullWikiHeaderShow', 
+    array('doc' => $doc, 'cursor' => 0)
+  ) ?>
 </div>
 
-<?php
-  if (isset($return_url)) 
-  {
-    echo ull_button_to(__('Link to this document', null, 'common'), $sf_data->getRaw('return_url'));
-    echo '<br /><br />';    
-  }
-?>
+<?php if (isset($return_url)): ?>  
+  <?php echo ull_button_to(__('Link to this document', null, 'common'), 
+    $sf_data->getRaw('return_url')) ?>
+  <br /><br />    
+<?php endif ?>
 
 <div class='ull_wiki_main'>
   <?php
+    $body = $sf_data->getRaw('doc')->body;
   
-  $body = $sf_data->getRaw('doc')->body;
+    $body = html_entity_decode($body, ENT_QUOTES, 'UTF-8');  
   
-  /*
-  // 's' extend the meaning of '.' beyond newlines
-  preg_match_all("#<pre>(.*?)</pre>#is", $body, $matches); 
+    $body = auto_link_text($body, $link = 'all', array(
+        'class'  => 'link_new_window',
+        'target' => '_blank',
+        'title'  => __('Link opens in a new window', null, 'common')
+    ));
+    
   
-//  preg_match_all("#<code>[^<]+</code>#", $body, $matches);
-
-//  ullCoreTools::printR($matches);
-
-  foreach($matches[1] as $match) {
-    
-    $replace = html_entity_decode($match, null, 'UTF-8');
-    $replace = str_replace('<br />', '', $replace);
-    //    $replace = str_replace('<pre>', '<code>', $replace);
-//    $replace = str_replace('</pre>', '</code>', $replace);
-
-    
-//    ullCoreTools::printR($replace);
-    $highlighter = new dkGeshi($replace, 'php');
-    $replace = $highlighter->parse_code();
-    
-//    $replace = str_replace('<pre>', '<code>', $replace);
-//    $replace = str_replace('</pre>', '</code>', $replace);
-    
-    $body = str_replace($match, $replace, $body);
-    
-  }
-*/
-
-  
-  
-if (!function_exists('u_func')) {
-  
-  function u_func($matches) {
-    
-//    ullCoreTools::printR($matches);
-    
-    $match = $matches[0];
-    
-    
-    
-//    echo "<textarea cols=80 rows=10>$match</textarea>";
-    
-    $replace = html_entity_decode($match, ENT_QUOTES, 'UTF-8');
-    
-//    echo "<textarea cols=80 rows=10>$replace</textarea>";
-    
-//    $replace = html_entity_decode($replace, ENT_QUOTES, 'UTF-8');
-//    
-//    echo "<textarea cols=80 rows=10>$replace</textarea>";
-    
-    $replace = str_replace('<br />', '', $replace);
-    $replace = str_replace("<pre>", '', $replace);
-    $replace = str_replace('</pre>', '', $replace);
-
-    
-//    ullCoreTools::printR($replace);
-    $highlighter = new dkGeshi($replace, 'php');
-    $replace = $highlighter->parse_code();
-    
-    return $replace;
-    
-  }
-
-}
-
-  
-  //$body = preg_replace_callback(
-  //          '#<pre>.*?</pre>#is'
-  //          , 'u_func'
-  //          , $body);
-  //INSTEAD:
-  $body = html_entity_decode($body, ENT_QUOTES, 'UTF-8');  
-
-  $body = auto_link_text($body, $link = 'all', array(
-      'class'  => 'link_new_window',
-      'target' => '_blank',
-      'title'  => __('Link opens in a new window', null, 'common')
-  ));
-  
-
-  echo $body;
-
-//  ullCoreTools::printR($body);
-
-//foreach($matches[1] as $match) {
-//....
-//}
-//   $highlighter = new dkGeshi(, 'php');
-//   echo $highlighter->parse_code();
-
-//  echo $ullwiki->getBody(); 
+    echo $body;
   ?>
 </div>
 
