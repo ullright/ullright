c42441309dec868a747a4e0586c852366e0026a5
Thomas Strauß <thomas@dekutree.net>
2010-07-07 13:35:36 +0000

    ullNews: open external links in new window

--- plugins/ullCmsPlugin/modules/ullNews/templates/_newsList.php
+++ plugins/ullCmsPlugin/modules/ullNews/templates/_newsList.php
@@ -1,11 +1,20 @@
 <div class="ull_news">
   <?php if ($generator->getRow()->exists()): ?>
     <?php foreach ($generator->getForms() as $row => $form): ?>
+     
+      <?php $link_options = array();?>
+      <?php if (substr($form['link_url']->render(), 0, 7) == 'http://')
+            {
+              echo $form['link_url']->render();
+              $link_options = array('link_new_window' => true, 'link_external' => true);
+            } 
+      ?>
+      
       <div class="ull_news_entry">
         <div class="ull_news_image">
           <?php if ($form['link_url']->render() && $form['image_upload']->render()): ?>
-            <?php echo link_to($form['image_upload']->render(), 
-              $form['link_url']->render()) ?>
+            <?php echo ull_link_to($form['image_upload']->render(), 
+              $form['link_url']->render(), $link_options) ?>
           <?php else: ?>
             <?php echo $form['image_upload']->render() ?>
           <?php endif ?>
@@ -24,8 +33,8 @@
           <p class="ull_news_abstract"><?php echo $form['abstract']->render() ?></p>
           <?php if ($form['link_name']->render() && $form['link_url']->render()): ?>
             <p class="ull_news_link">
-              <?php echo link_to($form['link_name']->render(), 
-               $form['link_url']->render()) ?>
+              <?php echo ull_link_to($form['link_name']->render(), 
+               $form['link_url']->render(), $link_options) ?>
             </p>
           <?php endif ?>
         </div>
