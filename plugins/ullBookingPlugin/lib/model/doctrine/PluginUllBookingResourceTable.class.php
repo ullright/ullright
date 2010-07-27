6e8c01434a1858d641a54307ed5efc87ab9fe424
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2010-07-27 13:05:47 +0000

    ullBooking: order booking resources alphabetically

--- plugins/ullBookingPlugin/lib/model/doctrine/PluginUllBookingResourceTable.class.php
+++ plugins/ullBookingPlugin/lib/model/doctrine/PluginUllBookingResourceTable.class.php
@@ -5,14 +5,20 @@ class PluginUllBookingResourceTable extends UllRecordTable
 {
   /**
    * Convenience function to find all booking resources
-   * with an enabled 'is_bookable' flag.
-   * 
+   * with an enabled 'is_bookable' flag, ordered by the
+   * (translated) name column.
+   *
    * Can be used as the table method option for sfWidgetFormDoctrineChoice.
-   *  
+   *
    * @return the results of findByIsBookable(true)
    */
   public function findBookableResources()
   {
-    return $this->findByIsBookable(true);
+    $q = new ullQuery('UllBookingResource');
+    $q
+      ->addWhere('x.is_bookable = ?', true)
+      ->addOrderBy('Translation->name')
+    ;
+    return $q->execute();
   }
 }
