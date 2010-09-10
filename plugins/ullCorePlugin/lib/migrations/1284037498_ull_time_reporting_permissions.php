460ee29997c1774a811dd491aa70f10ea17ab87c
Klemens Ullmann-Marx <klemens@ull.at>
2010-09-10 06:48:26 +0000

    Fixed ull_time_report permissions migration

--- plugins/ullCorePlugin/lib/migrations/1284037498_ull_time_reporting_permissions.php
+++ plugins/ullCorePlugin/lib/migrations/1284037498_ull_time_reporting_permissions.php
@@ -6,37 +6,24 @@ class ullTimeReportingPermissions extends Doctrine_Migration_Base
   {
     $dbh = Doctrine_Manager::getInstance()->getCurrentConnection()->getDbh();
     
-    //remove ull_time_report permission from TimeAdmin (if existing)
-    $result = $dbh->query("SELECT id FROM ull_permission WHERE slug = 'ull_time_report'");
-    $row = $result->fetch(PDO::FETCH_ASSOC);
-    $ullPermissionId = $row['id'];
-    $result = $dbh->query("SELECT id FROM ull_entity WHERE type='group' AND display_name = 'TimeAdmins'");
-    $row = $result->fetch(PDO::FETCH_ASSOC);
-    $timeAdminsId = $row['id'];
-
-    $query = $dbh->exec("DELETE FROM ull_group_permission WHERE ull_group_id = '"
-      . $timeAdminsId . "' and ull_permission_id = '" . $ullPermissionId . "'");
-
-    //now recreate the permission, but for logged-in users
+    // ull_time_report permission is now ull_time_report_all (the permission for admins)
+    $dbh->query("UPDATE ull_permission SET slug = 'ull_time_report_all' WHERE slug = 'ull_time_report'");
+    
+    /* now recreate the ull_time_report permission, but for logged-in users */
+    
     $result = $dbh->query("SELECT id FROM ull_entity WHERE type='group' AND display_name = 'Logged in users'");
     $row = $result->fetch(PDO::FETCH_ASSOC);
     $loggedUsersId = $row['id'];
-
-    $gp = new UllGroupPermission;
-    $gp->ull_permission_id = $ullPermissionId;
-    $gp->ull_group_id = $loggedUsersId;
-    $gp->save();
-
-    //add new ull_time_report_all permission
+    
+    //add new ull_time_report permission (general access to reports)
     $p = new UllPermission;
-    $p->slug = 'ull_time_report_all';
+    $p->slug = 'ull_time_report';
     $p->namespace = 'ull_time';
     $p->save();
 
-    //and grant it to TimeAdmins
     $gp = new UllGroupPermission;
     $gp->ull_permission_id = $p->id;
-    $gp->ull_group_id = $timeAdminsId;
+    $gp->ull_group_id = $loggedUsersId;
     $gp->save();
   }
 
