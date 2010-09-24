dab92b9be0f473526bede980be8303ec06f5e3a5
Klemens Ullmann-Marx <klemens@ull.at>
2010-09-24 13:35:16 +0000

    Fixed migration

--- plugins/ullCorePlugin/lib/migrations/1285334412_ull_project_is_routine_fix.php
+++ plugins/ullCorePlugin/lib/migrations/1285334412_ull_project_is_routine_fix.php
@@ -4,10 +4,13 @@ class UllProjectIsRoutineFix extends Doctrine_Migration_Base
 {
   public function up()
   {
+    $dbh = Doctrine_Manager::getInstance()->getCurrentConnection()->getDbh();
     $dbh->query("UPDATE ull_project SET is_routine = false WHERE is_routine IS NULL");
   }
 
   public function down()
   {
+    throw new Doctrine_Migration_IrreversibleMigrationException();
+    
   }
 }
