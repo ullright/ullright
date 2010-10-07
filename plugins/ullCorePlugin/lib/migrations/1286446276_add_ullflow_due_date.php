4fc0fe64cf69af5fd5567be56170081c911792bd
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2010-10-07 12:19:41 +0000

    Fixed wrong column name for new ullFlow:due_date migration

--- plugins/ullCorePlugin/lib/migrations/1286446276_add_ullflow_due_date.php
+++ plugins/ullCorePlugin/lib/migrations/1286446276_add_ullflow_due_date.php
@@ -5,12 +5,12 @@ class AddUllFlowDueDate extends Doctrine_Migration_Base
   public function up()
   {
     $this->addColumn('ull_flow_doc', 'due_date', 'datetime');
-    $this->addColumn('ull_flow_column_config', 'is_project', 'boolean');
+    $this->addColumn('ull_flow_column_config', 'is_due_date', 'boolean');
   }
 
   public function down()
   {
     $this->removeColumn('ull_flow_doc', 'due_date');
-    $this->removeColumn('ull_flow_column_config', 'is_project');
+    $this->removeColumn('ull_flow_column_config', 'is_due_date');
   }
 }
