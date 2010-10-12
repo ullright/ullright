e2ce8550fae5d911ae45bfa41b56759db9b56979
Thomas Strauß <thomas@dekutree.net>
2010-10-12 09:49:56 +0000

    ullOrgchart: test ullTreeNode assistant

--- test/unit/ullCorePlugin/ullTreeNodeOrgchartTest.php
+++ test/unit/ullCorePlugin/ullTreeNodeOrgchartTest.php
@@ -7,7 +7,7 @@ include dirname(__FILE__) . '/../../bootstrap/unit.php';
 //sfLoader::loadHelpers('ull');
 //sfLoader::loadHelpers('I18N');
 
-$t = new lime_test(6, new lime_output_color);
+$t = new lime_test(11, new lime_output_color);
 
 
 $t->diag('__construct()');
@@ -28,3 +28,17 @@ $t->diag('hasSubordinates()');
   $t->ok($node->hasSubordinates(), 'Node has subordinates');
   $node = new ullTreeNodeOrgchart('testdata');
   $t->is($node->hasSubordinates(), false, 'Node has no subordinates');
+  
+$t->diag('addAssistant/getAssistants()');
+  $node->addAssistant(new ullTreeNodeOrgchart('assistant1 data'));
+
+  $assistants= $node->getAssistants();
+  $t->ok(is_array($assistants), 'Returns an array of assistantss');
+  $t->is(count($assistants), 1, 'Returns one assistants');
+  $t->is(reset($assistants)->getData(), 'assistant1 data', 'Assistants contains the correct data');
+  
+  
+$t->diag('hasAssistants()');
+  $t->ok($node->hasAssistants(), 'Node has assistants');
+  $node = new ullTreeNodeOrgchart('testdata');
+  $t->is($node->hasAssistants(), false, 'Node has no assistants');
