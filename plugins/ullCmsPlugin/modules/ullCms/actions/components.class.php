efbd1ed76e68ea459287dcf88633a26acb2ff4e4
Klemens Ullmann-Marx <klemens@ull.at>
2010-12-01 15:28:20 +0000

    ullCms: quick fix for "is_current"

--- plugins/ullCmsPlugin/modules/ullCms/actions/components.class.php
+++ plugins/ullCmsPlugin/modules/ullCms/actions/components.class.php
@@ -2,11 +2,11 @@
 class ullCmsComponents extends sfComponents
 {
   
-  public function executeMainMenu()
+  public function executeMainMenu(sfRequest $request)
   {
     $navigation = UllCmsItemTable::getMenuTree(
       'main-menu', 
-      '', 
+      $request->getParameter('slug'),  //TODO: get the top level slug for subpages
       sfConfig::get('app_ull_cms_main_menu_depth', 2)
     );
     $this->setVar('main_menu', new ullTreeMenuRenderer($navigation, $this->getVar('renderUlTag')), true);    
