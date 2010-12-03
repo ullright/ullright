5881f5a00ae869679907626211a340b2ebc70b6c
Klemens Ullmann-Marx <klemens@ull.at>
2010-12-03 12:13:12 +0000

    ullMail: generated model classes

---
+++ plugins/ullMailPlugin/lib/filter/doctrine/PluginUllNewsletterInstanceCategoryFormFilter.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PluginUllNewsletterInstanceCategory form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage filter
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: sfDoctrineFormFilterPluginTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+abstract class PluginUllNewsletterInstanceCategoryFormFilter extends BaseUllNewsletterInstanceCategoryFormFilter
+{
+}
---
+++ plugins/ullMailPlugin/lib/form/doctrine/PluginUllNewsletterLayoutForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PluginUllNewsletterLayout form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage form
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: sfDoctrineFormPluginTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+abstract class PluginUllNewsletterLayoutForm extends BaseUllNewsletterLayoutForm
+{
+}
