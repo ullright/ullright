f8bc0a9735c76974a0bbdcfd14d5064a0ac7ba3f
Klemens Ullmann-Marx <klemens@ull.at>
2010-12-09 07:56:38 +0000

    Added css classes

--- plugins/ullCmsPlugin/modules/ullCms/templates/showSuccess.php
+++ plugins/ullCmsPlugin/modules/ullCms/templates/showSuccess.php
@@ -2,16 +2,20 @@
   <?php echo $sidebar_menu ?>
 <?php end_slot() ?>
 
-<h1>
-  <?php if ($allow_edit): ?>
-    <?php
-      echo ull_link_to(ull_image_tag('edit'),
-        array('module' => 'ullCms', 'action' => 'edit', 'id' => $doc->id));
-    ?>              
-  <?php endif ?>
-  <?php echo $title ?>
-</h1>
-
-<?php echo $body ?>
-
-<?php include_slot('ull_cms_additional_body') ?> 
+<div class="ull_cms_content">
+  <h1 class="ull_cms_content_heading">
+    <?php if ($allow_edit): ?>
+      <span class="ull_cms_content_heading_edit_link">
+        <?php
+          echo ull_link_to(ull_image_tag('edit'),
+            array('module' => 'ullCms', 'action' => 'edit', 'id' => $doc->id));
+        ?>   
+      </span>           
+    <?php endif ?>
+    <span class="ull_cms_content_heading_text"><?php echo $title ?></span>
+  </h1>
+  
+  <?php echo $body ?>
+  
+  <?php include_slot('ull_cms_additional_body') ?>
+</div> 
