8c85b83a4169bd69699e154ea9f5b6940ff0ee15
Thomas Strauß <thomas@dekutree.net>
2010-12-14 09:57:18 +0000

    ullright: display news on the startpage

--- apps/frontend/modules/myModule/templates/indexSuccess.php
+++ apps/frontend/modules/myModule/templates/indexSuccess.php
@@ -1,3 +1,12 @@
 <h1>Welcome, I hope you're ullright!</h1>
 <p>If you can see this, the basic configuration is working fine.</p>
 <p>Please login as user admin/admin.</p>
+
+<br></br>
+
+<h1>News:</h1>
+
+<div id="news">
+    <?php include_component('ullNews', 'newsList') ?>
+</div>
+
