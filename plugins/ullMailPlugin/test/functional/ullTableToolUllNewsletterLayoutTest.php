f8ef3d351765d04c3b76ad2aff574d6a79850c7d
Thomas Strauß <thomas@dekutree.net>
2010-12-15 13:12:43 +0000

    ullMail: added tests

---
+++ plugins/ullMailPlugin/test/functional/ullTableToolUllNewsletterLayoutTest.php
@@ -0,0 +1,37 @@
+<?php
+
+include dirname(__FILE__) . '/../../../../test/bootstrap/functional.php';
+
+$selector = new ullDomGridSelector('table.list_table > tbody', 'tr', 'td', array(),
+      array(
+        'edit_delete',      
+        'name'
+      )
+    );
+
+$b = new ullTableToolTestBrowser(
+  'UllNewsletterLayout', 
+  'Newsletter layout', 
+  'Manage Newsletter layout', 
+  0, 
+  $selector, 
+  $configuration
+);
+$path = dirname(__FILE__);
+$b->setFixturesPath($path);
+$b->resetDatabase();
+
+$createValues = array(
+  'name' => 'Test layout', 
+  'html_layout' => array('This is a test', false)
+);
+$editValues = array(
+  'name' => 'Test layout update', 
+  'html_layout' => array('Another test', false)
+);
+
+$b->setCreateValues($createValues);
+$b->setEditValues($editValues);
+$b->executeTest();
+
+
---
+++ plugins/ullMailPlugin/test/functional/ullTableToolUllNewsletterMailingListTest.php
@@ -0,0 +1,41 @@
+<?php
+
+include dirname(__FILE__) . '/../../../../test/bootstrap/functional.php';
+
+$selector = new ullDomGridSelector('table.list_table > tbody', 'tr', 'td', array(),
+      array(
+        'edit_delete',      
+        'name',
+        'description',
+        'is_active'
+      )
+    );
+
+$b = new ullTableToolTestBrowser(
+  'UllNewsletterMailingList', 
+  'Newsletter mailing list', 
+  'Manage Newsletter mailing list', 
+  0, 
+  $selector, 
+  $configuration
+);
+$path = dirname(__FILE__);
+$b->setFixturesPath($path);
+$b->resetDatabase();
+
+$createValues = array(
+  'name' => 'Test mailing list', 
+  'description' => 'This is a test',
+  'is_active' => array(true, 'Checkbox_checked')
+);
+$editValues = array(
+  'name' => 'Test mailing list 2', 
+  'description' => 'Another test',
+  'is_active' => array(false, 'Checkbox_unchecked')
+);
+
+$b->setCreateValues($createValues);
+$b->setEditValues($editValues);
+$b->executeTest();
+
+
