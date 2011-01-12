4fe5d04206db14d57558c53d4936d5467e1ea7bd
Thomas Strauß <thomas@dekutree.net>
2011-01-12 09:35:50 +0000

    ullCms: render cms page: check is_active status

--- plugins/ullCmsPlugin/modules/ullCms/lib/BaseUllCmsComponents.class.php
+++ plugins/ullCmsPlugin/modules/ullCms/lib/BaseUllCmsComponents.class.php
@@ -31,6 +31,7 @@ class BaseUllCmsComponents extends sfComponents
     $this->doc = Doctrine::getTable('UllCmsPage')->findOneBySlug($this->slug);
     $this->setVar('title', $this->doc->title);
     $this->setVar('body', $this->doc->body, true);
+    $this->setVar('is_active', $this->doc->is_active);
     
     $this->allow_edit = UllUserTable::hasPermission('ull_cms_edit');
   }
--- plugins/ullCmsPlugin/modules/ullCms/templates/_renderCmsPage.php
+++ plugins/ullCmsPlugin/modules/ullCms/templates/_renderCmsPage.php
@@ -1,20 +1,22 @@
-<div class="ull_cms_content_area">
-  <h1 class="ull_cms_content_area_heading">
-    <?php  if ($allow_edit): ?>
-      <span class="ull_cms_content_heading_edit_link">
-        <?php
-          echo ull_link_to(ull_image_tag('edit'),
-            array('module' => 'ullCms', 'action' => 'edit', 'id' => $doc->id));
-        ?>   
-      </span>           
-    <?php endif ?>
-    <span class="ull_cms_content_area_heading_text"><?php echo $title ?></span>
-  </h1>
-  
-  <?php echo auto_link_text($body, $link = 'all', array(
-        'class'  => 'link_external',
-        'target' => '_blank',
-  )); ?>
-  
-  <?php //include_slot('ull_cms_additional_body') ?>
-</div> 
+<?php if ($is_active): ?>
+  <div class="ull_cms_content_area">
+    <h1 class="ull_cms_content_area_heading">
+      <?php  if ($allow_edit): ?>
+        <span class="ull_cms_content_heading_edit_link">
+          <?php
+            echo ull_link_to(ull_image_tag('edit'),
+              array('module' => 'ullCms', 'action' => 'edit', 'id' => $doc->id));
+          ?>   
+        </span>           
+      <?php endif ?>
+      <span class="ull_cms_content_area_heading_text"><?php echo $title ?></span>
+    </h1>
+    
+    <?php echo auto_link_text($body, $link = 'all', array(
+          'class'  => 'link_external',
+          'target' => '_blank',
+    )); ?>
+    
+    <?php //include_slot('ull_cms_additional_body') ?>
+  </div> 
+<?php endif ?>
