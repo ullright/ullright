74512ffc42bd2b02f2d8c6487a425a81e6040c46
Klemens Ullmann-Marx <klemens@ull.at>
2011-01-14 09:58:43 +0000

    ullCore: navigation: added missing modules, arranged order

--- apps/frontend/modules/myModule/templates/_navTopLinks.php
+++ apps/frontend/modules/myModule/templates/_navTopLinks.php
@@ -1,9 +1,34 @@
-<ul class="nav_top_links">            
-  
-  <li id='nav_link_workflow'>
+<ul class="nav_top_links">        
+
+  <li id='nav_link_ull_mail'>
   <?php
-    echo ull_navigation_link('/ullFlowThemeNGPlugin/images/ull_flow_32x32',
-      'ullFlow/index', __('Workflows', null, 'common'));
+  echo ull_navigation_link('/ullMailThemeNGPlugin/images/ull_mail_32x32',
+      'ullNewsletter/index', __('Newsletter', null, 'ullMailMessages'),
+      array());
+  ?>
+  </li> 
+
+  <li id='nav_link_ull_ventory'>
+  <?php
+  echo ull_navigation_link('/ullVentoryThemeNGPlugin/images/ull_ventory_32x32',
+      'ullVentory/index', __('Inventory', null, 'ullVentoryMessages'),
+      array());
+  ?>
+  </li>    
+  
+  <li id='nav_link_ull_phone'>
+  <?php
+  echo ull_navigation_link('/ullPhoneThemeNGPlugin/images/ull_phone_32x32',
+      'ullPhone/list', __('Phone dir.', null, 'ullPhoneMessages'),
+      array());
+  ?>
+  </li>
+  
+  <li id='nav_link_ull_orgchart'>
+  <?php
+  echo ull_navigation_link('/ullPhoneThemeNGPlugin/images/ull_orgchart_32x32',
+      'ullOrgchart/list', __('Orgchart', null, 'ullOrgchartMessages'),
+      array());
   ?>
   </li>
   
@@ -13,13 +38,20 @@
       'ullWiki/list', __('Wiki', null, 'common'),
       array());
   ?>
-  </li>
+  </li>  
   
-  <li id='nav_link_ull_ventory'>
+  <li id='nav_link_ull_cms'>
   <?php
-  echo ull_navigation_link('/ullVentoryThemeNGPlugin/images/ull_ventory_32x32',
-      'ullVentory/index', __('Inventory', null, 'ullVentoryMessages'),
+  echo ull_navigation_link('/ullCmsThemeNGPlugin/images/ull_cms_32x32',
+      'ullCms/list', __('Content', null, 'ullCmsMessages'),
       array());
+  ?>
+  </li>   
+      
+  <li id='nav_link_workflow'>
+  <?php
+    echo ull_navigation_link('/ullFlowThemeNGPlugin/images/ull_flow_32x32',
+      'ullFlow/index', __('Workflows', null, 'common'));
   ?>
   </li>
   
@@ -31,21 +63,9 @@
   ?>
   </li>  
   
-  <li id='nav_link_ull_phone'>
-  <?php
-  echo ull_navigation_link('/ullPhoneThemeNGPlugin/images/ull_phone_32x32',
-      'ullPhone/list', __('Phone directory', null, 'ullPhoneMessages'),
-      array());
-  ?>
-  </li>
+
   
-  <li id='nav_link_ull_orgchart'>
-  <?php
-  echo ull_navigation_link('/ullPhoneThemeNGPlugin/images/ull_orgchart_32x32',
-      'ullOrgchart/list', __('Orgchart', null, 'ullOrgchartMessages'),
-      array());
-  ?>
-  </li>  
+     
   
   <?php if (UllUserTable::hasGroup('MasterAdmins') || UllUserTable::hasGroup('UserAdmins')): ?>
     <li id="nav_link_admin">
