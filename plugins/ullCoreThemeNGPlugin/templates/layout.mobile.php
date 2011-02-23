eb145691579f94172b044fb8ffc7d527db67081a
Klemens Ullmann-Marx <klemens@ull.at>
2011-02-23 14:14:24 +0000

    Added comments to default layouts

--- plugins/ullCoreThemeNGPlugin/templates/layout.mobile.php
+++ plugins/ullCoreThemeNGPlugin/templates/layout.mobile.php
@@ -1,19 +1,42 @@
-<?php require('_head.mobile.php') ?>
+<!-- Define statements to be added to the html head  -->
+<?php slot('html_head') ?>
 
+  <!-- Include rss feed (Uncomment to activate) -->
+  <?php //include_component('ullNews', 'rssFeed') ?>
+
+  <!-- Add a global custom stylesheet (Uncomment to activate) -->
+  <!-- Put the file in web/css/ -->
+  <?php //use_stylesheet('/css/my_stylesheet.css', 'last', array('media' => 'all')) ?>
+  
+  <!-- Add a global custom javascript (Uncomment to activate) -->
+  <!-- Put the file in web/js/ -->
+  <?php //use_javascript('/js/my_javascript.js') ?>
+  
+<?php end_slot() ?>
+
+<!-- Actually include the html head --> 
+<?php require(sfConfig::get('sf_plugins_dir') . 
+  '/ullCoreTheme' .
+  sfConfig::get('app_theme_package', 'NG') .
+  'Plugin/templates/_head.mobile.php') ?>
+
+<!--  Begin of html body including css selectors for the current page -->
+<!--  Example: <body class="ullCms_show"> -->
 <body class="<?php 
   echo sfInflector::underscore($sf_context->getModuleName()) . '_' . $sf_context->getActionName();
 ?>">
 
+<!-- Top-level box containing all subsequent elements -->
 <div id="container">
-
   
-  <!-- Contains main navigation, content and footer -->
+  <!-- The canvas contains main navigation, content and footer -->
   <div id="canvas">
   
+    <!-- Navigation -->  
     <div id="nav_top">
-    
+      
+      <!-- Logo -->
       <span id="nav_logo">
-        <?php //echo link_to(image_tag('/ullCoreThemeNGPlugin/images/logo_75'), '@homepage') ?>
           <?php echo ull_link_to(
         image_tag(sfConfig::get('app_sidebar_logo',
           '/ullCoreThemeNGPlugin/images/logo_120'), 'alt="logo"')
@@ -22,22 +45,21 @@
         ); ?>
       </span>
   
-<!--      <div id="nav_syslinks_container">-->
-      
-        <ul class="nav_syslinks_list">
-          <?php include_component('ullUser', 'headerLogin') ?>
-          
-          <?php include_component('ullUser', 'headerSyslinkMyAccount') ?>
-              
-          <?php include_component('ullUser', 'headerSyslinkLanguageSelectionGermanEnglish') ?>        
-        </ul>
+      <!-- Login, "My Account", langugage selection -->
+      <ul class="nav_syslinks_list">
+        <?php include_component('ullUser', 'headerLogin') ?>
         
-<!--      </div>  end of nav_syslinks_container -->
+        <?php include_component('ullUser', 'headerSyslinkMyAccount') ?>
+            
+        <?php include_component('ullUser', 'headerSyslinkLanguageSelectionGermanEnglish') ?>        
+      </ul>
       
-      
+      <!-- Main navigation links (ullright modules) -->
       <div id='nav_links'>             
         <?php include_partial('myModule/navTopLinks'); ?>
       </div>
+      
+      <!-- Example for an ullCms main menu (Uncomment to activate) -->
       <!-- 
       <div class='nav_main_menu'>
         <ul class="ull_menu_main_menu">
@@ -46,23 +68,31 @@
       </div>
       -->
       
-    </div> <!-- end of nav_top -->
+    <!-- End of navigation -->  
+    </div>
+    
      
-     
+    <!-- Content box --> 
     <div id="content">
       <?php echo $sf_data->getRaw('sf_content') ?>
     </div> <!-- end of content -->
+    
      
+    <!-- Sidebar box -->     
     <div id="sidebar">
       <?php include_partial('default/sidebar_inclusion') ?>
     </div> 
-     
+    
+    
+    <!-- Footer -->
     <div id="footer">
+    
       <div id="footer_copyright">
         © 2007-<?php echo date('Y')?> 
         by <a href="mailto:klemens.ullmann-marx@ull.at">Klemens Ullmann-Marx</a>
         / <?php echo ull_link_to('ull.at', 'http://www.ull.at', 'ull_js_observer_confirm=true link_external=true')?>
       </div>
+      
       <div id="footer_links">
         Powered by the enterprise 2.0 platform 
         <?php echo ull_link_to(
@@ -77,13 +107,17 @@
           'ull_js_observer_confirm=true'
         ) ?>
       </div>
-      <!-- <div class='clear'></div> --> 
-    </div>  <!-- end of footer -->
-    
+      
+    <!-- End of footer -->  
+    </div>
+  
+  <!-- End of canavas --> 
   </div> <!-- end of canavas -->
-    
-</div> <!--  end of container -->
 
+<!-- End of container -->    
+</div> 
+
+<!--  End of html body -->
 </body>
 
 </html>
--- plugins/ullCoreThemeNGPlugin/templates/layout.php
+++ plugins/ullCoreThemeNGPlugin/templates/layout.php
@@ -1,65 +1,94 @@
-<?php require('_head.php') ?>
+<!-- Define statements to be added to the html head  -->
+<?php slot('html_head') ?>
 
+  <!-- Include rss feed (Uncomment to activate) -->
+  <?php //include_component('ullNews', 'rssFeed') ?>
+
+  <!-- Add a global custom stylesheet (Uncomment to activate) -->
+  <!-- Put the file in web/css/ -->
+  <?php //use_stylesheet('/css/my_stylesheet.css', 'last', array('media' => 'all')) ?>
+  
+  <!-- Add a global custom javascript (Uncomment to activate) -->
+  <!-- Put the file in web/js/ -->
+  <?php //use_javascript('/js/my_javascript.js') ?>
+  
+<?php end_slot() ?>
+
+
+<!-- Actually include the html head --> 
+<?php require(sfConfig::get('sf_plugins_dir') . 
+  '/ullCoreTheme' .
+  sfConfig::get('app_theme_package', 'NG') .
+  'Plugin/templates/_head.php') ?>
+
+<!--  Begin of html body including css selectors for the current page -->
+<!--  Example: <body class="ullCms_show"> -->
 <body class="<?php 
   echo sfInflector::underscore($sf_context->getModuleName()) . '_' . $sf_context->getActionName();
 ?>">
 
+<!-- The normal html markup starts here. This can be completely customized --> 
+
+<!-- Top-level box containing all subsequent elements -->
 <div id="container">
 
+  <!-- Sidebar box -->
   <div id="sidebar">
     
     <div id="sidebar_logo">
-    <?php echo ull_link_to(
-          image_tag(sfConfig::get('app_sidebar_logo',
-            '/ullCoreThemeNGPlugin/images/logo_120'), 'alt="logo"')
-            , '@homepage'
-            , 'ull_js_observer_confirm=true'
-          ); ?>
+      <?php echo ull_link_to(
+        image_tag(sfConfig::get('app_sidebar_logo',
+          '/ullCoreThemeNGPlugin/images/logo_120'), 'alt="logo"')
+          , '@homepage'
+          , 'ull_js_observer_confirm=true'
+        ) ?>
     </div>
    
     <?php include_partial('default/sidebar_inclusion') ?>
-   
-  </div> <!-- end of sidebar -->
   
-  <!-- Contains main navigation, content and footer -->
+  <!-- End of sidebar --> 
+  </div> 
+  
+  <!-- The canvas contains main navigation, content and footer -->
+  <!-- So basically it's everything except the sidebar -->
   <div id="canvas">
   
+    <!-- Navigation -->
     <div id="nav_top">
   
+      <!-- Main navigation links (ullright module icons) -->
       <div id='nav_links'>             
         <?php include_partial('myModule/navTopLinks'); ?>
       </div> 
       
+      <!-- A box on the right side containing e.g. the login link -->
       <div id="nav_syslinks_container">
       
+        <!-- A box containing "My Account" link and language selection -->
         <div id="nav_syslinks">
           <ul class="nav_syslinks_list">
-          
             <?php include_component('ullUser', 'headerSyslinkMyAccount'); ?>
-            
             <?php include_component('ullUser', 'headerSyslinkLanguageSelectionGermanEnglish'); ?>
-            
           </ul>
         </div>   
         
-        <!-- 
-        <div id="nav_searchbox">
-          <?php //include_component('ullWiki', 'headerSearch'); ?>
-        </div> 
-        -->
-          
+        <!-- A box containing the login link -->
         <div id="nav_loginbox">
           <ul class="nav_syslinks_list">
             <?php include_component('ullUser', 'headerLogin'); ?>
           </ul>
         </div>        
-        
-      </div> <!-- end of nav_syslinks_container -->
+      
+      <!-- End of nav_syslinks_container -->  
+      </div> 
+      
       
       <div id="nav_top_separator_line"></div>      
    
-      <div class='clear_right'></div><!-- to force the parent-box to enclose the floating divs -->
+      <!-- Force the parent-box ("nav_top") to enclose the floating divs -->
+      <div class='clear_right'></div>
       
+      <!-- Example for an ullCms main menu (Uncomment to activate) -->
       <!-- 
       <div class='nav_main_menu'>
         <ul class="ull_menu_main_menu">
@@ -68,19 +97,25 @@
       </div>
       -->
             
-    </div> <!-- end of nav_top -->
+    <!-- End of navigation -->            
+    </div> 
      
-     
+    
+    <!-- Content box --> 
+    <!-- The actual content of a page is rendered here. 
+         Think of it like the picture in a picture frame -->
     <div id="content">
       <?php echo $sf_data->getRaw('sf_content') ?>
-    </div> <!-- end of content -->
+    </div> 
      
-     
+    <!-- Footer --> 
     <div id="footer">
+    
       <div id="footer_copyright">
         © 2007-<?php echo date('Y')?> 
         by <?php echo ull_link_to('ull.at', 'http://www.ull.at', 'ull_js_observer_confirm=true link_external=true')?>
       </div>
+      
       <div id="footer_links">
         Powered by the enterprise 2.0 platform 
         <?php echo ull_link_to(
@@ -95,17 +130,23 @@
           'ull_js_observer_confirm=true'
         ) ?>
       </div>
-      <!-- <div class='clear'></div> --> 
-    </div>  <!-- end of footer -->
     
-  </div> <!-- end of canavas -->
-    
-</div> <!--  end of container -->
+    <!-- End of footer -->  
+    </div>  
+  
+  <!-- End of canavas -->  
+  </div> 
 
+<!-- End of container -->    
+</div> 
+
+
+<!-- Sidebar hide / unhide code -->
 <?php if (sfConfig::get('app_sidebar_toggle', true) == true) : ?>
   <?php include_partial('default/sidebar_toggle') ?>
 <?php endif; ?>
 
+<!--  End of html body -->
 </body>
 
 </html>
