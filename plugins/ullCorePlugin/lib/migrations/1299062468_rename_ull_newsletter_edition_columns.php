72f945baf84c9d8d55edfcc90efb55dd866f3747
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2011-03-02 12:17:43 +0000

    foreign key management: made task useable from outside, fixed most recent migration

--- plugins/ullCorePlugin/lib/migrations/1299062468_rename_ull_newsletter_edition_columns.php
+++ plugins/ullCorePlugin/lib/migrations/1299062468_rename_ull_newsletter_edition_columns.php
@@ -2,16 +2,22 @@
 
 class RenameUllNewsletterEditionColumns extends Doctrine_Migration_Base
 {
-  public function up()
+  public function preUp()
   {
-    //this foreign key name valid everywhere?
-    $this->dropForeignKey('ull_newsletter_edition',
-    	'ull_newsletter_edition_sent_by_ull_user_id_ull_entity_id');
-    
+    RecreateForeignKeysTask::dropAllForeignKeys();
+  }
+  
+  public function up()
+  {  
     $this->renameColumn('ull_newsletter_edition', 'sent_at', 'submitted_at');
     $this->renameColumn('ull_newsletter_edition', 'sent_by_ull_user_id', 'submitted_by_ull_user_id');
     $this->renameColumn('ull_newsletter_edition', 'num_sent_emails', 'num_total_recipients');
     $this->addColumn('ull_newsletter_edition', 'num_sent_recipients', 'integer');
+  }
+  
+  public function postUp()
+  {
+    RecreateForeignKeysTask::createAllForeignKeysFromModel();
   }
 
   public function down()
--- plugins/ullCorePlugin/lib/task/RecreateForeignKeysTask.class.php
+++ plugins/ullCorePlugin/lib/task/RecreateForeignKeysTask.class.php
@@ -31,16 +31,73 @@ EOF;
       'The environment', 'cli');
   }
 
-
-  protected function execute($arguments = array(), $options = array())
+  public static function dropAllForeignKeys()
   {
-    $this->initializeDatabaseConnection($arguments, $options);
-    
     //get the current schema name
     $connection = Doctrine_Manager::connection();
     $info = Doctrine_Manager::getInstance()->parsePdoDsn($connection->getOption('dsn'));
     $dbName = $info['dbname'];
+    
+    //retrieve all foreign key names from the information_schema
+    $sql =
+          'SELECT table_name, constraint_name ' .
+          'FROM information_schema.referential_constraints ' .
+          "WHERE unique_constraint_schema = '" . $dbName . "'";
 
+    $constraints = $connection->fetchAll($sql);
+    foreach ($constraints as $constraint)
+    {
+      $connection->export->dropForeignKey($constraint['table_name'], $constraint['constraint_name']);
+    }
+
+    return count($constraints);
+  }
+  
+  public static function createAllForeignKeysFromModel()
+  {
+    Doctrine::loadModels(sfConfig::get('sf_root_dir') . '/lib/model/doctrine');
+    $models = Doctrine_Core::getLoadedModels();
+    //this call is needed to load behaviors (adds FKs for translation, version, ...) 
+    $models = Doctrine_Core::initializeModels($models);
+    
+    //create a list of foreign key definitions ...
+    $foreignKeys = array();
+    foreach ($models as $modelName)
+    {
+      $table = Doctrine::getTable($modelName);
+      $export = $table->getExportableFormat(true);
+
+      $foreignKeys[$export['tableName']] = $export['options']['foreignKeys'];
+    }
+
+    //retrieve Doctrine connection object
+    $connection = Doctrine_Manager::connection();
+      
+    //... and iterate through it
+    $counters = array('ok' => 0, 'error' => array());
+    foreach ($foreignKeys as $tableName => $foreignKeyDefinitions)
+    {
+      foreach ($foreignKeyDefinitions as $foreignKeyDefinition)
+      {
+        try
+        {
+          $connection->export->createForeignKey($tableName, $foreignKeyDefinition);
+          $counters['ok'] = $counters['ok'] + 1;
+        }
+        catch (Exception $e)
+        {
+          $counters['error'][] = var_export($foreignKeyDefinition, true);
+        }
+      }
+    }
+    
+    return $counters;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $this->initializeDatabaseConnection($arguments, $options);
+   
     $answer = $this->askConfirmation(array('This command will drop all foreign keys from the schema:',
       '', $dbName, '', 'Are you sure you want to proceed? (y/N)'), 'QUESTION_LARGE', false);
 
@@ -48,63 +105,25 @@ EOF;
     {
       //drop all foreign keys
       $this->logSection($this->name, 'Dropping all foreign keys ...');
-
-      //retrieve all foreign key names from the information_schema
-      $sql =
-          'SELECT table_name, constraint_name ' .
-          'FROM information_schema.referential_constraints ' .
-          "WHERE unique_constraint_schema = '" . $dbName . "'";
-
-      $constraints = $connection->fetchAll($sql);
-      foreach ($constraints as $constraint)
-      {
-        $connection->export->dropForeignKey($constraint['table_name'], $constraint['constraint_name']);
-      }
-
-      $this->logSection($this->name, 'Dropped ' . count($constraints) . ' foreign keys');
-
+      $droppedCount = self::dropAllForeignKeys();
+      $this->logSection($this->name, 'Dropped ' . $droppedCount . ' foreign keys');
+      
+      
       //recreate all foreign keys
       $this->logSection($this->name, 'Recreating foreign keys from model ...');
-
-      Doctrine::loadModels(sfConfig::get('sf_root_dir') . '/lib/model/doctrine');
-      $models = Doctrine_Core::getLoadedModels();
-      //this call is needed to load behaviors (adds FKs for translation, version, ...) 
-      $models = Doctrine_Core::initializeModels($models);
-
-      //create a list of foreign key definitions ...
-      $foreignKeys = array();
-      foreach($models as $modelName)
+      $counters = self::createAllForeignKeysFromModel($models);       
+      $this->logSection($this->name, 'Recreated ' . $counters['ok'] . ' foreign keys from model');
+      
+      if (count($counters['error']) > 0)
       {
-        $table = Doctrine::getTable($modelName);
-        $export = $table->getExportableFormat(true);
-
-        $foreignKeys[$export['tableName']] = $export['options']['foreignKeys'];
-      }
-
-      //... and iterate through it
-      $i = 0; $errors = 0;
-      foreach ($foreignKeys as $tableName => $foreignKeyDefinitions)
-      {
-        foreach ($foreignKeyDefinitions as $foreignKeyDefinition)
+        $this->logSection($this->name, 'Failed to create ' .
+          count($counters['error']) . ' foreign key(s)', null, ERROR);
+        
+        foreach ($counters['error'] as $error)
         {
-          try
-          {
-            $connection->export->createForeignKey($tableName, $foreignKeyDefinition);
-            $i++;
-          }
-          catch (Exception $e)
-          {
-            $this->logBlock(array('Error while creating foreign key for table: ' . $tableName,
-              'Tried to create the following definition:', var_export($foreignKeyDefinition, true)), ERROR);
-            $errors++;
-          }
+          $this->logBlock(array('Error while creating foreign key,' .
+            ' tried to create the following definition:', $error), ERROR);
         }
-      }
-
-      $this->logSection($this->name, 'Recreated ' . $i . ' foreign keys from model');
-      if ($errors > 0)
-      {
-        $this->logSection($this->name, 'Failed to create ' . $errors . ' foreign key(s)', null, ERROR);
       }
       
       $this->logSection($this->name, 'Finished');
