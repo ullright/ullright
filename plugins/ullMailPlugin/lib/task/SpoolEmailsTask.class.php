e7e659b83546d5f439c16426ceceb611233b6f56
Martin Leonhartsberger-Schrott <martin@dekutree.net>
2011-03-04 14:22:23 +0000

    SpoolEmailsTask: added locking to prevent problems when task execution is not serialized

--- plugins/ullMailPlugin/lib/task/SpoolEmailsTask.class.php
+++ plugins/ullMailPlugin/lib/task/SpoolEmailsTask.class.php
@@ -28,17 +28,40 @@ EOF;
     $this->initializeDatabaseConnection($arguments, $options);
     $context = sfContext::createInstance($this->configuration);
     
+    //important note concerning Doctrine locking
+    //the locking manager has a serious bug: row-level locking is not possible,
+    //every time an object is locked the whole table (= model) gets locked.
+    //the upside: for our purposes this behavior is 'good enough'
+    
+    //create locking manager and unique id for locking
+    //pid + random string should be unique enough
+    $lockingManager = new Doctrine_Locking_Manager_Pessimistic(Doctrine_Manager::connection());
+    $uniqueId = uniqid(getmypid(), true);
+    
+    //remove old newsletter edition locks (> 15 min)
+    $table = Doctrine_Core::getTable('UllNewsletterEdition');
+    $lockingManager->releaseAgedLocks(900, $table->getComponentName());
+    
     $editions = UllNewsletterEditionTable::findEditionsToBeSpooled();
     
     if (!count($editions))
     {
       $this->log('No editions found to be spooled!');
+      return;
     }
     
     $failedRecipients = array();
     
     foreach ($editions as $edition)
     {
+      //request exclusive lock for the newsletter object
+      if (!$this->requestLock($lockingManager, $edition, $uniqueId))
+      {
+        $this->log('Could not retrieve lock for newsletter edition with id: ' .
+          $edition['id']);
+        continue;
+      }
+      
       $this->logBlock('Now spooling ' . $edition['subject'], 'INFO');
       
       $context->getUser()->setCulture($edition['sender_culture']);
@@ -56,35 +79,80 @@ EOF;
       //TODO: add handling for multiple UllUsers for batchSend
       $numSent = 0;
       
-      $edition['queued_at'] = date('Y-m-d H:i:s');
-      $edition->save();
+      $connection = Doctrine_Manager::connection();
+      $transaction = $connection->beginTransaction();
+      $transaction = $connection->transaction;
+      $transaction->setIsolation('SERIALIZABLE');
       
-      foreach ($recipients as $recipient)
+      $this->log('Beginning to spool');
+      
+      try
       {
-        $this->log('...for ' . $recipient['first_name'] . ' ' . $recipient['last_name']);
-        
-        $currentMail = clone $mail;
-        $currentMail->clearRecipients(); // TODO: why is this necessary despite cloning?
-        
-        try 
+        foreach ($recipients as $recipient)
         {
-          $currentMail->addAddress($recipient);
-          $this->getMailer()->sendQueue($currentMail);
-          $numSent++;
-        }
-        catch (Exception $e)
-        {
-          $this->log('Invalid address: ' . $recipient['email']);
-          $failedRecipients[] = $recipient['email'];
-        }
-      }  
+          $this->log('...for ' . $recipient['first_name'] . ' ' . $recipient['last_name']);
+          
+          $currentMail = clone $mail;
+          $currentMail->clearRecipients(); // TODO: why is this necessary despite cloning?
+          
+          try 
+          {
+            $currentMail->addAddress($recipient);
+            $this->getMailer()->sendQueue($currentMail);
+            $numSent++;
+          }
+          catch (Exception $e)
+          {
+            $this->log('Invalid address: ' . $recipient['email']);
+            $failedRecipients[] = $recipient['email'];
+          }
+        }  
+        
+        $edition['queued_at'] = date('Y-m-d H:i:s');
+        $edition->save();
+        
+        $this->log('Comitting all spooled messages');
+        $connection->commit();
+      }
+      catch (Exception $e)
+      {
+        $this->log('Exception during spooling, executing rollback');
+        $connection->rollback();
+        
+        $this->releaseLock($lockingManager, $edition, $uniqueId);
+        throw $e;
+      }
+      
+      $this->releaseLock($lockingManager, $edition, $uniqueId);
       
       $this->logBlock("Queued {$numSent} of " . count($recipients) . ' messages', 'INFO');
       $this->log('Failed Recipients: ' . count($failedRecipients));
       $this->log(print_r($failedRecipients, true));
-    }      
-    
-    
+    }        
   }
   
+  protected function requestLock($lockingManager, $edition, $uniqueId)
+  {
+    try
+    {
+      return $lockingManager->getLock($edition, $uniqueId);
+    }
+    catch (Doctrine_Locking_Exception $dle)
+    {
+      $this->log($dle->getMessage());
+      return false;
+    }
+  }
+  
+  protected function releaseLock($lockingManager, $edition, $uniqueId)
+  {
+    try
+    {
+      $lockingManager->releaseLock($edition, $uniqueId);
+    }
+    catch(Doctrine_Locking_Exception $dle)
+    {
+      $this->log($dle->getMessage());
+    }
+  }
 }
