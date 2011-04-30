f6290d22cd9709c6f56f6788bfe588e9f99e7633
Klemens Ullmann-Marx <klemens@ull.at>
2011-04-30 06:40:36 +0000

    Fixed migration for additional UllUser fields

--- plugins/ullCorePlugin/lib/migrations/1304062858_add_ull_user_personal_contact_columns.php
+++ plugins/ullCorePlugin/lib/migrations/1304062858_add_ull_user_personal_contact_columns.php
@@ -2,17 +2,30 @@
 
 class AddUllUserPersonalContactColumns extends Doctrine_Migration_Base
 {
+  protected $tableNames = array(
+    'ull_entity',
+    'ull_entity_version',
+    'ull_user_version',
+    'ull_group_version',
+    'ull_ventory_origin_dummy_user_version',
+    'ull_ventory_status_dummy_user_version',
+    'ull_clone_user_version'
+  );  
+  
   public function up()
   {
-    $this->addColumn('ull_entity', 'title', 'string', 100);
-    $this->addColumn('ull_entity', 'birth_date', 'date');
-    $this->addColumn('ull_entity', 'street', 'string', 200);
-    $this->addColumn('ull_entity', 'post_code', 'string', 10);
-    $this->addColumn('ull_entity', 'city', 'string', 100);
-    $this->addColumn('ull_entity', 'country', 'string', 10);
-    $this->addColumn('ull_entity', 'phone_number', 'string', 20);
-    $this->addColumn('ull_entity', 'fax_number', 'string', 20);
-    $this->addColumn('ull_entity', 'website', 'string', 255);
+    foreach ($this->tableNames as $tableName)
+    {
+      $this->addColumn($tableName, 'title', 'string', 100);
+      $this->addColumn($tableName, 'birth_date', 'date');
+      $this->addColumn($tableName, 'street', 'string', 200);
+      $this->addColumn($tableName, 'post_code', 'string', 10);
+      $this->addColumn($tableName, 'city', 'string', 100);
+      $this->addColumn($tableName, 'country', 'string', 10);
+      $this->addColumn($tableName, 'phone_number', 'string', 20);
+      $this->addColumn($tableName, 'fax_number', 'string', 20);
+      $this->addColumn($tableName, 'website', 'string', 255);
+    }
   }
 
   public function down()
