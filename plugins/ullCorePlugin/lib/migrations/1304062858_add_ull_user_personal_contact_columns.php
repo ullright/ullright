f8c208263199351aff2caf525bf4da5645eee40d
Klemens Ullmann-Marx <klemens@ull.at>
2011-05-02 14:05:48 +0000

    fixed migration

--- plugins/ullCorePlugin/lib/migrations/1304062858_add_ull_user_personal_contact_columns.php
+++ plugins/ullCorePlugin/lib/migrations/1304062858_add_ull_user_personal_contact_columns.php
@@ -14,18 +14,45 @@ class AddUllUserPersonalContactColumns extends Doctrine_Migration_Base
   
   public function up()
   {
+    $dbh = Doctrine_Manager::getInstance()->getCurrentConnection()->getDbh();
+    
+    $columns = array(
+      array('title', 'string', 100),
+      array('birth_date', 'date'),
+      array('street', 'string', 200),
+      array('post_code', 'string', 10),
+      array('city', 'string', 100),
+      array('country', 'string', 10),
+      array('phone_number', 'string', 20),
+      array('fax_number', 'string', 20),
+      array('website', 'string', 255),
+    );    
+    
+    // Fix leftover error from failed migration
     foreach ($this->tableNames as $tableName)
     {
-      $this->addColumn($tableName, 'title', 'string', 100);
-      $this->addColumn($tableName, 'birth_date', 'date');
-      $this->addColumn($tableName, 'street', 'string', 200);
-      $this->addColumn($tableName, 'post_code', 'string', 10);
-      $this->addColumn($tableName, 'city', 'string', 100);
-      $this->addColumn($tableName, 'country', 'string', 10);
-      $this->addColumn($tableName, 'phone_number', 'string', 20);
-      $this->addColumn($tableName, 'fax_number', 'string', 20);
-      $this->addColumn($tableName, 'website', 'string', 255);
-    }
+      foreach ($columns as $column)
+      {    
+        try
+        {
+          $result = $dbh->query("SELECT $column[0] FROM $tableName LIMIT 1");
+        }
+        catch (Exception $e)
+        {
+          // If the column does not exist yet -> add it
+          if (!isset($column[2]))
+          {
+            $this->addColumn($tableName, $column[0], $column[1]);
+          }
+          else
+          {
+            $this->addColumn($tableName, $column[0], $column[1], $column[2]);
+          }
+          var_dump('added column ' . $column[0] . ' for ' . $tableName);
+        }    
+      }
+    }      
+    
   }
 
   public function down()
