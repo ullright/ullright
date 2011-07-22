4de6f8a58afd86f2524bb68e402101c908f44d09
Klemens Ullmann-Marx <klemens@ull.at>
2011-07-22 12:49:43 +0000

    Improved migration

--- plugins/ullCorePlugin/lib/migrations/1311325305_drop_ull_flow_app_permission.php
+++ plugins/ullCorePlugin/lib/migrations/1311325305_drop_ull_flow_app_permission.php
@@ -11,10 +11,45 @@ class DropUllFlowAppPermission extends Doctrine_Migration_Base
   {
     $dbh = Doctrine_Manager::getInstance()->getCurrentConnection()->getDbh();
     
-    $dbh->query("DELETE FROM ull_permission WHERE slug='ullFlow_trouble_ticket_global_read'");
-    $dbh->query("DELETE FROM ull_permission WHERE slug='ullFlow_trouble_ticket_global_write'");
+    // This is kind of brutal, because we delete versions....
+    // TODO: fix versionable delete 
+    // http://www.ullright.org/ullFlow/edit/app/bug_tracking/order/priority/order_dir/asc/doc/1533  
+    $result = $dbh->query("SELECT id FROM ull_permission WHERE slug='ullFlow_trouble_ticket_global_read'");
+    $data = $result->fetch(PDO::FETCH_ASSOC);
+    $readId = $data['id'];
     
-    RecreateForeignKeysTask::createAllForeignKeysFromModel();
+    $result = $dbh->query("SELECT id FROM ull_permission WHERE slug='ullFlow_trouble_ticket_global_write'");
+    $data = $result->fetch(PDO::FETCH_ASSOC);
+    $writeId = $data['id'];    
+
+    $dbh->query("DELETE FROM ull_group_permission_version WHERE ull_permission_id=$readId");
+    $dbh->query("DELETE FROM ull_group_permission_version WHERE ull_permission_id=$writeId");    
+    
+    $dbh->query("DELETE FROM ull_group_permission WHERE ull_permission_id=$readId");
+    $dbh->query("DELETE FROM ull_group_permission WHERE ull_permission_id=$writeId");
+    
+    $dbh->query("DELETE FROM ull_permission_version WHERE slug='ullFlow_trouble_ticket_global_read'");
+    $dbh->query("DELETE FROM ull_permission_version WHERE slug='ullFlow_trouble_ticket_global_write'");    
+    
+    $dbh->query("DELETE FROM ull_permission WHERE slug='ullFlow_trouble_ticket_global_read'");
+    $dbh->query("DELETE FROM ull_permission WHERE slug='ullFlow_trouble_ticket_global_write'");    
+    
+    $libPath = sfConfig::get('sf_lib_dir');
+    
+    echo shell_exec('svn --force delete ' . $libPath . '/model/doctrine/ullFlowPlugin/base/BaseUllFlowAppPermission.class.php');
+    echo shell_exec('svn --force delete ' . $libPath . '/model/doctrine/ullFlowPlugin/UllFlowAppPermission.class.php');
+    echo shell_exec('svn --force delete ' . $libPath . '/model/doctrine/ullFlowPlugin/UllFlowAppPermissionTable.class.php');
+    
+    echo shell_exec('svn --force delete ' . $libPath . '/form/doctrine/ullFlowPlugin/base/BaseUllFlowAppPermissionForm.class.php');
+    echo shell_exec('svn --force delete ' . $libPath . '/form/doctrine/ullFlowPlugin/UllFlowAppPermissionForm.class.php');
+    
+    echo shell_exec('svn --force delete ' . $libPath . '/filter/doctrine/ullFlowPlugin/base/BaseUllFlowAppPermissionFormFilter.class.php');
+    echo shell_exec('svn --force delete ' . $libPath . '/filter/doctrine/ullFlowPlugin/UllFlowAppPermissionFormFilter.class.php');
+    
+    echo shell_exec('php symfony cache:clear');
+    echo shell_exec('php symfony doctrine:build --model --forms --filters');
+    
+    RecreateForeignKeysTask::createAllForeignKeysFromModel();    
   }  
   
   public function down()
