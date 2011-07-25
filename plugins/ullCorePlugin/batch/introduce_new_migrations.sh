49662df4c31ed0951848fb66cf8000fd689a7ec0
Klemens Ullmann-Marx <klemens@ull.at>
2011-07-25 08:52:47 +0000

    typo

--- plugins/ullCorePlugin/batch/introduce_new_migrations.sh
+++ plugins/ullCorePlugin/batch/introduce_new_migrations.sh
@@ -17,7 +17,7 @@ svn --force delete lib/filter/doctrine/ullFlowPlugin/UllFlowAppPermissionFormFil
 php symfony cache:clear
 php symfony doctrine:build --model --forms --filters
 
-cd lib/migrations
+cd lib/migration
 ln -s ../../plugins/ullCorePlugin/lib/migrations_data/ data
 ln -s ../../plugins/ullCorePlugin/lib/migrations_pre_build_model/ pre_build_model
 svn add *
