0d9b672d6e917d5d96bc995a97d6e77a279de62d
Klemens Ullmann-Marx <klemens@ull.at>
2011-07-29 09:36:43 +0000

    Fixed pre build model migration to use a file instead of the database

--- plugins/ullCorePlugin/lib/PreBuildModel_Doctrine_Migration.class.php
+++ plugins/ullCorePlugin/lib/PreBuildModel_Doctrine_Migration.class.php
@@ -2,5 +2,57 @@
 
 class PreBuildModel_Doctrine_Migration extends Doctrine_Migration
 {
-   protected $_migrationTableName = 'migration_version_pre_build_model';
+  
+  /**
+   * Get the path for the version store file
+   */
+  public function getVersionFile()
+  {
+    return sfConfig::get('sf_data_dir') . '/pre_build_model_version.txt';
+  }
+   
+  /**
+   * Set the current version of the database
+   *
+   * @param integer $number
+   * @return void
+   */
+  public function setCurrentVersion($number)
+  {
+    file_put_contents($this->getVersionFile(), $number);
+  }
+
+  /**
+   * Get the current version of the database
+   *
+   * @return integer $version
+   */
+  public function getCurrentVersion()
+  {
+    $result = null;
+    
+    if (file_exists($this->getVersionFile()))
+    {
+      $result = (integer) file_get_contents($this->getVersionFile());
+    }
+
+    return ($result) ? $result : 0;
+  }
+
+  /**
+   * Returns true/false for whether or not this database has been migrated in the past
+   *
+   * @return boolean $migrated
+   */
+  public function hasMigrated()
+  {
+    $result = null;
+    
+    if (file_exists($this->getVersionFile()))
+    {
+      $result = (integer) file_get_contents($this->getVersionFile());
+    }
+
+    return ($result) ? true : false;
+  }
 }
