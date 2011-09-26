9ee593e88aec0b9dfc2be10606242549dccea97f
Klemens Ullmann-Marx <klemens@ull.at>
2011-09-26 13:00:36 +0000

    Fixed ullCourse migration to be executed only when using ullCourse plugin

--- plugins/ullCorePlugin/lib/migrations_data/1315900115_default_ull_course_is_bookable.php
+++ plugins/ullCorePlugin/lib/migrations_data/1315900115_default_ull_course_is_bookable.php
@@ -5,13 +5,19 @@ class DefaultUllCourseIsBookable extends Doctrine_Migration_Base
   public function up()
   {
     
-   $q = new Doctrine_Query;
-   $q
-    ->update('UllCourse c')
-    ->set('c.is_bookable', '?', true)
-  ;
-  
-   $num = $q->execute(); //$num = number of updated rows   
+    // Load the ullCourse data only if the module is enabled
+    $enabledModules = sfConfig::get('sf_enabled_modules');
+    
+    if (in_array('ullCourse', $enabledModules))
+    {    
+      $q = new Doctrine_Query;
+      $q
+        ->update('UllCourse c')
+        ->set('c.is_bookable', '?', true)
+      ;
+    
+      $num = $q->execute(); //$num = number of updated rows   
+    }
     
   }
 
