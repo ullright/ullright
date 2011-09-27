9b5bed5257439e6f60a63bbc414028d4889fc86e
Klemens Ullmann-Marx <klemens@ull.at>
2011-09-27 11:41:43 +0000

    Fixed test

--- plugins/ullCorePlugin/test/unit/ullTableTool/ullMetaWidgetUllEntityTest.php
+++ plugins/ullCorePlugin/test/unit/ullTableTool/ullMetaWidgetUllEntityTest.php
@@ -38,7 +38,7 @@ $t->diag('write access with default options');
   $t->isa_ok($widget, 'ullMetaWidgetUllEntity', '__construct() returns the correct object');
   $widget->addToFormAs('my_field');
   $t->isa_ok($form->getWidgetSchema()->offsetGet('my_field'), 'ullWidgetFormChoiceUllEntity', 'returns the correct widget for write access');
-  $t->isa_ok($form->getValidatorSchema()->offsetGet('my_field'), 'sfValidatorChoice', 'returns the correct validator for write access');
+  $t->isa_ok($form->getValidatorSchema()->offsetGet('my_field'), 'ullValidatorUllEntity', 'returns the correct validator for write access');
   $t->is($widget->getColumnConfig()->getOption('entity_classes'), array('UllUser', 'UllGroup'), 'returns the correct list of default entity classes');
   
 $t->diag('write access with custom entity classes');
