ed45a8ec4c036fa46be58026df8bb27147ec0cde
Klemens Ullmann-Marx <klemens@ull.at>
2011-11-15 09:02:57 +0000

    added docblock

--- plugins/ullMailPlugin/lib/model/doctrine/PluginUllNewsletterEditionTable.class.php
+++ plugins/ullMailPlugin/lib/model/doctrine/PluginUllNewsletterEditionTable.class.php
@@ -7,31 +7,31 @@
  */
 class PluginUllNewsletterEditionTable extends UllRecordTable
 {
-    /**
-     * Returns an instance of this class.
-     *
-     * @return object PluginUllNewsletterEditionTable
-     */
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('PluginUllNewsletterEdition');
-    }
+  /**
+   * Returns an instance of this class.
+   *
+   * @return object PluginUllNewsletterEditionTable
+   */
+  public static function getInstance()
+  {
+    return Doctrine_Core::getTable('PluginUllNewsletterEdition');
+  }
 
+  
+  /**
+   * Find editions which are ready to be spooled
+   * 
+   */
+  public static function findEditionsToBeSpooled()
+  {
+    $q = new Doctrine_Query;
+    $q
+      ->from('UllNewsletterEdition e')
+      ->where('e.submitted_at IS NOT NULL')
+      ->addWhere('e.queued_at IS NULL')
+      ->addWhere('e.is_active = ?', true)
+    ;
     
-    /**
-     * Find editions which are ready to be spooled
-     * 
-     */
-    public static function findEditionsToBeSpooled()
-    {
-      $q = new Doctrine_Query;
-      $q
-        ->from('UllNewsletterEdition e')
-        ->where('e.submitted_at IS NOT NULL')
-        ->addWhere('e.queued_at IS NULL')
-        ->addWhere('e.is_active = ?', true)
-      ;
-      
-      return $q->execute();
-    }
+    return $q->execute();
+  }
 }
