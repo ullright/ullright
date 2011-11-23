fde0896e048eeb80ec921ad44db51b721f80884a
Klemens Ullmann-Marx <klemens@ull.at>
2011-11-23 08:18:17 +0000

    Removed unnecessary admin links

--- plugins/ullCmsPlugin/modules/ullCms/templates/_adminLinks.php
+++
@@ -1,5 +0,0 @@
-<h3><?php echo __('Content Mangement', null, 'ullCmsMessages') ?></h3>
-<ul class="tc_tasks">
-  <li><?php echo ull_tc_task_link('/ullCmsThemeNGPlugin/images/ull_cms_24x24', 'ullCms/list', __('Manage', null, 'common') . ' ' . __('Pages', null, 'ullCmsMessages')) ?></li>
-  <li><?php echo ullTableConfiguration::renderTaskCenterLink('UllCmsMenuItem', 'ullCms', 'ull_cms_24x24') ?></li>
-</ul>
--- plugins/ullCmsPlugin/modules/ullNews/templates/_adminLinks.php
+++
@@ -1,4 +0,0 @@
-<h3><?php echo __('Homepage News', null, 'ullCmsMessages') ?></h3>
-<ul class="tc_tasks">
-  <li><?php echo ull_tc_task_link('/ullCmsThemeNGPlugin/images/ull_cms_24x24', 'ullNews/list', __('Manage', null, 'common') . ' ' . __('News entries', null, 'ullNewsMessages')) ?></li>
-</ul> 
