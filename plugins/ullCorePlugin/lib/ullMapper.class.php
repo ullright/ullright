ffacf53756840147fd379a42d888288dad20ef0e
Klemens Ullmann-Marx <klemens@ull.at>
2011-12-19 10:26:03 +0000

    Added a ullMapper class to map a input array to an output array according to a given mapping

---
+++ plugins/ullCorePlugin/lib/ullMapper.class.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * Map the keys of a source array to a target array with keys according to the 
+ * given mapping.
+ * 
+ * Discards unmapped fields in the source data.
+ * 
+ * @see unit test ullMapperTest.php for examples 
+ * @author klemens.ullmann-marx@ull.at
+ */
+class ullMapper
+{
+  protected
+    $data = array(),
+    $mapping = array(),
+    $errors = array()
+  ;
+  
+ 
+  /**
+   * Constructor 
+   *
+   * @param array $data     The input data
+   * @param array $mapping  The mapping with format "input key" => "output key"
+   */
+  public function __construct(array $data, array $mapping)
+  {
+    $this->data = $data;
+    $this->mapping = $mapping;
+  }
+  
+  /**
+   * Check if the mapping produced errors
+   * 
+   * @return: boolean
+   */
+  public function hasErrors()
+  {
+    return (boolean) count($this->errors);
+  }
+  
+  /**
+   * Get an array of errors
+   * 
+   * @return: array
+   */
+  public function getErrors()
+  {
+    return $this->errors;
+  }
+  
+  /**
+   * Perform the mapping
+   * 
+   * @return: array
+   */
+  public function doMapping()
+  {
+    $result = array();
+    
+    foreach ($this->data as $line)
+    {
+      $lineResult = array();
+      
+      foreach ($line as $name => $value)
+      {
+        if (in_array($name, $this->getMappingSourceFields()))
+        {
+          $lineResult[$this->mapping[$name]] = $value;
+        }
+        else
+        {
+          $this->errors[$name] = __(
+            'Warning: no column "%column%" found', 
+            array('%column%' => $name),
+            'ullCoreMessages'
+          );
+        }
+      } // end of foreach field
+      
+      // Make sure we have all target fields in the output array      
+      $lineResult = array_merge(
+        $this->getEmptyTargetLine(),
+        $lineResult
+      );
+      
+      $result[] = $lineResult;
+    } // end of foreach rows
+    
+    return $result;
+  }
+  
+  /**
+   * Helper method to get only the source fields
+   * (The left part of the mapping)
+   * 
+   * @return: array
+   */
+  public function getMappingSourceFields()
+  {
+    return array_keys($this->mapping);
+  }
+  
+  
+  /**
+   * Create an empty target line array
+   * 
+   * Format: targetFieldName => null
+   * 
+   * @return: array
+   */
+  public function getEmptyTargetLine()
+  {
+    $result = array();
+    
+    foreach ($this->mapping as $targetFieldName)
+    {
+      $result[$targetFieldName] = null;
+    }
+    
+    return $result;
+  } 
+  
+  
+}
---
+++ plugins/ullCorePlugin/test/unit/ullMapperTest.php
@@ -0,0 +1,94 @@
+<?php
+
+include dirname(__FILE__) . '/../../../../test/bootstrap/unit.php';
+
+sfContext::createInstance($configuration);
+sfContext::getInstance()->getConfiguration()->loadHelpers('I18N');
+
+$t = new lime_test(6, new lime_output_color);
+
+$t->diag('__construct()');
+
+  $data = array(
+    array(
+      'First name'  => 'Klemens',
+      'Last name'   => 'Ullmann-Marx',
+      'Email'       => 'k@ull.at',
+      'Mailing list'=> 'Product news',
+    ),
+    array(
+      'First name'  => 'Trailing',
+      'Last name'   => 'Charm',
+      'Invalid column' => 'foobar',
+    ),
+  );
+  
+  $mapping = array(
+    'First name'    => 'first_name',
+    'Last name'     => 'last_name',
+    'Email'         => 'email',
+    'Mailing list'  => 'UllNewsletterMailingList'
+  );
+  
+  $mapper = new ullMapper($data, $mapping);
+
+
+$t->diag('hasErrors() before doMapping()');  
+  
+  $t->is($mapper->hasErrors(), false, 'No errors');
+  
+  
+ $t->diag('doMapping()');   
+  
+  $reference = array(
+    array(
+      'first_name'  => 'Klemens',
+      'last_name'   => 'Ullmann-Marx',
+      'email'       => 'k@ull.at',
+      'UllNewsletterMailingList' => 'Product news',
+    ),
+    array(
+      'first_name'  => 'Trailing',
+      'last_name'   => 'Charm',
+      'email'       => null,
+      'UllNewsletterMailingList' => null,
+    )
+  );
+  
+  $t->is($mapper->doMapping(), $reference, 'Performs the correct mapping');
+  
+  
+$t->diag('has/getErrors() after doMapping() with errors'); 
+  
+  $t->is($mapper->hasErrors(), true, 'Now we have errors');
+  
+  $reference = array(
+    'Invalid column' => 'Warning: no column "Invalid column" found',
+  );
+  
+  $t->is($mapper->getErrors(), $reference, 'Returns the correct error');
+
+  
+$t->diag('getMappingSourceFields()');  
+  
+  $reference = array(
+    'First name',
+    'Last name',
+    'Email',
+    'Mailing list',
+  );
+  
+  $t->is($mapper->getMappingSourceFields(), $reference, 'Return the correct source columns');  
+  
+  
+$t->diag('getEmptyTargetLine()');
+
+  $reference = array(
+    'first_name'    => null,
+    'last_name'     => null,
+    'email'         => null,
+    'UllNewsletterMailingList'  => null,
+  );
+  
+  $t->is($mapper->getEmptyTargetLine(), $reference, 'Returns the correct result');
+  
