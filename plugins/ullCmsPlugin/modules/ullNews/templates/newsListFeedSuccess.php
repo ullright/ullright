3edbedc24f79471a2ed6e22aee1230fdcfb2dd21
Klemens Ullmann-Marx <klemens@ull.at>
2012-05-04 12:22:12 +0000

    ullNews: new config option for rss news feed to confige the link

--- plugins/ullCmsPlugin/modules/ullNews/templates/newsListFeedSuccess.php
+++ plugins/ullCmsPlugin/modules/ullNews/templates/newsListFeedSuccess.php
@@ -1,20 +1,22 @@
 <?php echo '<?xml version="1.0" encoding="utf-8"?>' ?>
 
+<?php $link = sfConfig::get('app_ull_news_rss_link', '@homepage') ?>
+
 <feed xmlns="http://www.w3.org/2005/Atom">
   <author>
     <name><?php echo sfConfig::get('app_ull_news_rss_author') ?></name>
   </author>
   <title><?php echo __(sfConfig::get('app_ull_news_rss_title', 'ullright newsfeed')) ?></title>
   <link rel="self" href="<?php echo $sf_request->getUri()?>" />
-  <id><?php echo url_for('@homepage', true). 'feed' ?></id>
+  <id><?php echo url_for($link, true). 'feed' ?></id>
   <logo><?php echo sfConfig::get('app_ull_news_rss_logo') ?></logo>
   <updated><?php echo gmstrftime('%Y-%m-%dT%H:%M:%SZ', Doctrine::getTable('UllNews')->findLatestNews()->getDateTimeObject('activation_date')->format('U')) ?></updated>
 
   <?php foreach($newsEntries as $newsEntry): ?>
   <entry>
     <title><?php echo $newsEntry['title'] ?></title>
-    <link href="<?php echo url_for('@homepage', true) ?>"/>
-    <id><?php echo url_for('@homepage', true) . 'feed/' . $newsEntry['slug'] ?></id>
+    <link href="<?php echo url_for($link, true) ?>"/>
+    <id><?php echo url_for($link, true) . 'feed/' . $newsEntry['slug'] ?></id>
     <updated><?php echo gmstrftime('%Y-%m-%dT%H:%M:%SZ', Doctrine::getTable('UllNews')->findOneBySlug($newsEntry['slug'])->getDateTimeObject('activation_date')->format('U')) ?></updated>
     <summary type="html"><![CDATA[
       <p><?php echo nl2br($newsEntry['abstract']) ?></p>
