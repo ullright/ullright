7a34d570b0b9c1bef3013635fa39fff7bb9f74b4
Klemens Ullmann-Marx <klemens@ull.at>
2012-06-21 08:12:33 +0000

    _headerLogin component: added css classes

--- plugins/ullCorePlugin/modules/ullUser/templates/_headerLogin.php
+++ plugins/ullCorePlugin/modules/ullUser/templates/_headerLogin.php
@@ -1,10 +1,12 @@
-<li>
-  <?php if ($username): ?>
+<?php if ($username): ?>
+  <li id="logged_in_as">
     <?php echo __('Logged in as', null, 'common') ?>
     <?php echo ull_link_entity_popup($username, $sf_user->getAttribute('user_id')) ?>
     -
     <?php echo ull_link_to(__('Log out', null, 'common'), 'ullUser/logout', 'ull_js_observer_confirm=true') ?>
-<?php else: ?>
-    <?php echo ull_link_to(__('Log in', null, 'common'), 'ullUser/login', 'ull_js_observer_confirm=true') ?>
-<?php endif ?>
   </li>
+<?php else: ?>
+  <li id="log_in">
+    <?php echo ull_link_to(__('Log in', null, 'common'), 'ullUser/login', 'ull_js_observer_confirm=true') ?>
+  </li>
+<?php endif ?>
