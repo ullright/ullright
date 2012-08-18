a76b050d0990c056a55effb4ef499eecdf4e5500
Klemens Ullmann-Marx <klemens@ull.at>
2012-08-18 09:03:12 +0000

    mobile version

--- plugins/ullCmsPlugin/modules/ullCms/templates/_switch.php
+++ plugins/ullCmsPlugin/modules/ullCms/templates/_switch.php
@@ -1,8 +1,10 @@
 <?php // Switch between inline editing view and visitor view ?>
 
-<?php use_stylesheet('/ullCmsThemeNGPlugin/css/inline_editing.css', 'first') ?>
-
 <?php if (UllUserTable::hasPermission('ull_cms_edit')): ?>
+
+  <?php use_stylesheet('/ullCmsThemeNGPlugin/css/inline_editing.css', 'first') ?>
+
+
   <div id="ull_cms_switch">
     <?php echo link_to_function(
       ull_image_tag('edit'), 
@@ -15,41 +17,43 @@
       array('id' => 'ull_cms_switch_off')
     ) ?>    
   </div>
-<?php endif ?>
 
-<?php $inline_editing = $sf_user->getAttribute('inline_editing') ?>
 
-<?php if ($inline_editing): ?>
-  <?php echo javascript_tag('
-    $(document).ready(function() {
+  <?php $inline_editing = $sf_user->getAttribute('inline_editing') ?>
+  
+  <?php if ($inline_editing): ?>
+    <?php echo javascript_tag('
+      $(document).ready(function() {
+        $("#ull_cms_switch_on").hide();
+      });
+    ') ?>
+  <?php else: ?>
+    <?php echo javascript_tag('
+      $(document).ready(function() {
+        $("#ull_cms_switch_off").hide();
+        $(".inline_editing").hide();
+      });
+    ') ?>
+  <?php endif  ?>
+      
+  <?php echo javascript_tag('    
+    function switchEditOn() {
+      $(".inline_editing").show();
+      $("#ull_cms_switch_off").show();
       $("#ull_cms_switch_on").hide();
-    });
-  ') ?>
-<?php else: ?>
-  <?php echo javascript_tag('
-    $(document).ready(function() {
-      $("#ull_cms_switch_off").hide();
+      $.ajax({
+        url: "' . url_for('ullCms/toggleInlineEditing?inline_editing=1') . '",
+      });        
+    }
+      
+    function switchEditOff() {
       $(".inline_editing").hide();
-    });
+      $("#ull_cms_switch_off").hide();
+      $("#ull_cms_switch_on").show();   
+      $.ajax({
+        url: "' . url_for('ullCms/toggleInlineEditing?inline_editing=0') . '",
+      });        
+    }    
   ') ?>
-<?php endif  ?>
     
-<?php echo javascript_tag('    
-  function switchEditOn() {
-    $(".inline_editing").show();
-    $("#ull_cms_switch_off").show();
-    $("#ull_cms_switch_on").hide();
-    $.ajax({
-      url: "' . url_for('ullCms/toggleInlineEditing?inline_editing=1') . '",
-    });        
-  }
-    
-  function switchEditOff() {
-    $(".inline_editing").hide();
-    $("#ull_cms_switch_off").hide();
-    $("#ull_cms_switch_on").show();   
-    $.ajax({
-      url: "' . url_for('ullCms/toggleInlineEditing?inline_editing=0') . '",
-    });        
-  }    
-') ?>
+<?php endif ?>    
