6c3573c4b4527633b380a34baf1dd26e2a8c8ee0
Klemens Ullmann-Marx <klemens@ull.at>
2012-08-26 12:41:50 +0000

    Added patch to output string diff using "colordiff" cmd command

--- lib/vendor/symfony/lib/vendor/lime/constraint/LimeConstraintIs.php
+++ lib/vendor/symfony/lib/vendor/lime/constraint/LimeConstraintIs.php
@@ -10,6 +10,28 @@
  * with this source code in the file LICENSE.
  */
 
+function diff($old, $new){
+foreach($old as $oindex => $ovalue){
+$nkeys = array_keys($new, $ovalue);
+foreach($nkeys as $nindex){
+$matrix[$oindex][$nindex] = isset($matrix[$oindex - 1][$nindex - 1]) ?
+$matrix[$oindex - 1][$nindex - 1] + 1 : 1;
+if($matrix[$oindex][$nindex] > $maxlen){
+$maxlen = $matrix[$oindex][$nindex];
+$omax = $oindex + 1 - $maxlen;
+$nmax = $nindex + 1 - $maxlen;
+}
+}	
+}
+if($maxlen == 0) return array(array('d'=>$old, 'i'=>$new));
+return array_merge(
+diff(array_slice($old, 0, $omax), array_slice($new, 0, $nmax)),
+array_slice($new, $nmax, $maxlen),
+diff(array_slice($old, $omax + $maxlen), array_slice($new, $nmax + $maxlen)));
+}
+
+require(sfConfig::get('sf_plugins_dir') . '/ullCorePlugin/lib/vendor/finediff/finediff.php');
+
 /**
  * Tests that a value equals another.
  *
@@ -31,7 +53,29 @@ class LimeConstraintIs extends LimeConstraint
     }
     catch (LimeAssertionFailedException $e)
     {
-      throw new LimeConstraintException(sprintf("     got: %s\nexpected: %s", $e->getActual(10), $e->getExpected(10)));
+      $file1 = sfConfig::get('sf_cache_dir') . '/lime_diff1.txt';
+      $file2 = sfConfig::get('sf_cache_dir') . '/lime_diff2.txt';
+      
+      $from = substr($e->getActual(), 1, -1);
+      $to = substr($e->getExpected(), 1, -1);
+      
+      file_put_contents($file1, $from);
+      file_put_contents($file2, $to);
+      
+      $diff = shell_exec('colordiff ' . $file1 . ' ' . $file2);
+      
+      unlink($file1);
+      unlink($file2);
+
+      echo "Colordiff output for the following assertion:\n" . $diff;
+      
+      $text = sprintf(
+        "     got: %s\nexpected: %s", 
+        $e->getActual(10), 
+        $e->getExpected(10)
+      );
+      
+      throw new LimeConstraintException($text);
     }
   }
 }
