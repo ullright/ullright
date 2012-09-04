5cb3e25e2be9193e1279569536dbd51240f57cca
Klemens Ullmann-Marx <klemens@ull.at>
2012-09-04 12:18:46 +0000

    ullUser: added event notifies for log in/out

--- plugins/ullCmsPlugin/config/ullCmsPluginConfiguration.class.php
+++ plugins/ullCmsPlugin/config/ullCmsPluginConfiguration.class.php
@@ -8,6 +8,21 @@ class ullCmsPluginConfiguration extends sfPluginConfiguration
   
   public function initialize()
   {
+    $this->dispatcher->connect(
+      'ull_user.log_in_success', 
+      array($this, 'resetInlineEditingToggle')
+    );
   }
+  
+  /** 
+   * Always turn on inline editing after login
+   *  
+   * @param sfEvent $event
+   */
+  public static function resetInlineEditingToggle(sfEvent $event)
+  {
+    sfContext::getInstance()->getUser()->setAttribute('inline_editing', true);
+  }
+  
   
 }
