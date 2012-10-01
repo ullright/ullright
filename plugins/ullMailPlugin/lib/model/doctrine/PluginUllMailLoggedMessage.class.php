4a0a3516b0c2301edacaa6d0b3b7a243c31adb34
Klemens Ullmann-Marx <klemens@ull.at>
2012-10-01 10:55:12 +0000

    ullMail: removed dead code

--- plugins/ullMailPlugin/lib/model/doctrine/PluginUllMailLoggedMessage.class.php
+++ plugins/ullMailPlugin/lib/model/doctrine/PluginUllMailLoggedMessage.class.php
@@ -85,30 +85,6 @@ abstract class PluginUllMailLoggedMessage extends BaseUllMailLoggedMessage
       $loggedMessage['plaintext_body'] = $mail->getBody();
     }
 
-//    //... and the bodies. Depending on the content type of the message,
-//    //HTML and plaintext bodies are extracted
-//
-//    if ($mail->isHtml)
-//    
-//    $contentType = $mail->getContentType();
-//    switch ($contentType)
-//    {
-//      case 'text/plain':
-//        $loggedMessage['plaintext_body'] = $mail->getBody();
-//        break;
-//
-//      case 'text/html':
-//        $loggedMessage['html_body'] = $mail->getBody();
-//        break;
-//
-//      default:
-//        if (strpos($contentType, 'multipart/') === 0)
-//        {
-//          $loggedMessage->parseMimeBodyParts($mail);
-//        }
-//        break;
-//    }
-
     // handle newsletter edition 
     if ($mail instanceof ullsfMail && $mail->getNewsletterEditionId())
     {
@@ -118,53 +94,4 @@ abstract class PluginUllMailLoggedMessage extends BaseUllMailLoggedMessage
     return $loggedMessage;
   }
 
-  /**
-   * Parses plaintext and HTML bodies from a Swift_Message
-   * instance. Handles plaintext only, HTMl only, and both.
-   * 
-   * @param Swift_Message $mail
-   */
-//  protected function parseMimeBodyParts(Swift_Message $mail)
-//  {
-//    $children = $mail->getChildren();
-//    foreach ($children as $child)
-//    {
-//      if ($child instanceof Swift_MimePart)
-//      {
-//        if ($child->getContentType() == 'text/plain')
-//        {
-//          $plainBody = $child->getBody();
-//        }
-//        else if ($child->getContentType() == 'text/html')
-//        {
-//          $htmlBody = $child->getBody();
-//        }
-//      }
-//    }
-//
-//    //we assume that if only a text/plain part exists,
-//    //the main body of the message is text/html; and
-//    //if only a text/html part exists, the main body
-//    //is text/plain - third case is both parts set
-//    if (isset($plainBody))
-//    {
-//      if (isset($htmlBody))
-//      {
-//        //both bodies are MIME parts
-//        $this['plaintext_body'] = $plainBody;
-//        $this['html_body'] = $htmlBody;
-//        return;
-//      }
-//
-//      //only the plain part is set
-//      $this['plaintext_body'] = $plainBody;
-//      $this['html_body'] = $mail->getBody();
-//    }
-//    else
-//    {
-//      //only the html part is set
-//      $this['html_body'] = $htmlBody;
-//      $this['plaintext_body'] = $mail->getBody();
-//    }
-//  }
 }
