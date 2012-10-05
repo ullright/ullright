ea9218ac733061d457c0c9963d7769d40cdb5fc8
Klemens Ullmann-Marx <klemens@ull.at>
2012-10-05 06:16:19 +0000

    Fixed migration

--- plugins/ullCorePlugin/lib/migrations_data/1345714390_ull_cms_default_page_content_type.php
+++ plugins/ullCorePlugin/lib/migrations_data/1345714390_ull_cms_default_page_content_type.php
@@ -4,15 +4,20 @@ class UllCmsDefaultPageContentType extends Doctrine_Migration_Base
 {
   public function up()
   {
-    $defaultId = Doctrine::getTable('UllCmsContentType')->findOneBySlug('default')->id;
+    $default = Doctrine::getTable('UllCmsContentType')->findOneBySlug('default');
     
-    $q = new Doctrine_Query;
-    $q
-      ->update('UllCmsPage p')
-      ->set('ull_cms_content_type_id', '?', $defaultId)
-      ->where('ull_cms_content_type_id IS NULL')
-    ;
-    $q->execute(); 
+    if ($default)
+    {
+      $defaultId = $default->id;
+  
+      $q = new Doctrine_Query;
+      $q
+        ->update('UllCmsPage p')
+        ->set('ull_cms_content_type_id', '?', $defaultId)
+        ->where('ull_cms_content_type_id IS NULL')
+      ;
+      $q->execute();
+    } 
   }
 
   public function down()
